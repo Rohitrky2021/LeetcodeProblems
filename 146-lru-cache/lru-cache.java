2305634eba0cdc3a6b2e82d7e41b2bd9d8df88cc
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-07-18 07:39:26 +0530

    Time: 77 ms (25.54%) | Memory: 120.1 MB (61.06%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,112 +1,80 @@
-import java.util.Hashtable;
 
 
-public class LRUCache {
+class Node {
+    public int key;
+    public int val;
+    public Node next;
+    public Node prev;
 
-class DLinkedNode {
-  int key;
-  int value;
-  DLinkedNode pre;
-  DLinkedNode post;
-}
-
-/**
- * Always add the new node right after head;
- */
-private void addNode(DLinkedNode node) {
-    
-  node.pre = head;
-  node.post = head.post;
-
-  head.post.pre = node;
-  head.post = node;
-}
-
-/**
- * Remove an existing node from the linked list.
- */
-private void removeNode(DLinkedNode node){
-  DLinkedNode pre = node.pre;
-  DLinkedNode post = node.post;
-
-  pre.post = post;
-  post.pre = pre;
-}
-
-/**
- * Move certain node in between to the head.
- */
-private void moveToHead(DLinkedNode node){
-  this.removeNode(node);
-  this.addNode(node);
-}
-
-// pop the current tail. 
-private DLinkedNode popTail(){
-  DLinkedNode res = tail.pre;
-  this.removeNode(res);
-  return res;
-}
-
-private Hashtable<Integer, DLinkedNode> 
-  cache = new Hashtable<Integer, DLinkedNode>();
-private int count;
-private int capacity;
-private DLinkedNode head, tail;
-
-public LRUCache(int capacity) {
-  this.count = 0;
-  this.capacity = capacity;
-
-  head = new DLinkedNode();
-  head.pre = null;
-
-  tail = new DLinkedNode();
-  tail.post = null;
-
-  head.post = tail;
-  tail.pre = head;
-}
-
-public int get(int key) {
-
-  DLinkedNode node = cache.get(key);
-  if(node == null){
-    return -1; // should raise exception here.
-  }
-
-  // move the accessed node to the head;
-  this.moveToHead(node);
-
-  return node.value;
-}
-
-
-public void put(int key, int value) {
-  DLinkedNode node = cache.get(key);
-
-  if(node == null){
-
-    DLinkedNode newNode = new DLinkedNode();
-    newNode.key = key;
-    newNode.value = value;
-
-    this.cache.put(key, newNode);
-    this.addNode(newNode);
-
-    ++count;
-
-    if(count > capacity){
-      // pop the tail
-      DLinkedNode tail = this.popTail();
-      this.cache.remove(tail.key);
-      --count;
+    public Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+        next = null;
+        prev = null;
     }
-  }else{
-    // update the value.
-    node.value = value;
-    this.moveToHead(node);
-  }
 }
 
+class LRUCache {
+    private Map<Integer, Node> cache;
+    private Node head;
+    private Node tail;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new HashMap<>();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void deleteNode(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void addNode(Node newNode) {
+        Node temp = head.next;
+        head.next = newNode;
+        newNode.prev = head;
+        newNode.next = temp;
+        temp.prev = newNode;
+    }
+
+    public int get(int key) {
+        if (!cache.containsKey(key))
+            return -1;
+
+        Node node = cache.get(key);
+        deleteNode(node);
+        addNode(node);
+        cache.put(key, head.next);
+        return head.next.val;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            deleteNode(node);
+            node.val = value;
+            addNode(node);
+            cache.put(key, head.next);
+        } else {
+            if (cache.size() == capacity) {
+                Node prev = tail.prev;
+                deleteNode(prev);
+                cache.remove(prev.key);
+                Node newNode = new Node(key, value);
+                addNode(newNode);
+                cache.put(key, head.next);
+            } else {
+                Node newNode = new Node(key, value);
+                addNode(newNode);
+                cache.put(key, head.next);
+            }
+        }
+    }
 }
