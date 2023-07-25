b12e138e5623b7128042c0b26931e6e5a02f8378
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-07-25 12:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (42.11%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,51 +1,12 @@
 class Solution {
-    public int missingNumber(int[] arr) {
-        int max=findMax(arr);
-        if(arr.length<max){
-            return max;
-        }
-                if(arr.length>max){
-            return max+1;
-        }
-        //                 if(arr.length==max){
-        //     return max+1;
-        // }
-        // if(isSorted(arr)){
-        //     return arr[arr.length-1]+1;
-        // }
-        int xor=0^0;
-        for(int i=0;i<arr.length;i++){
-            xor^=arr[i];
-        }
-                for(int i=0;i<arr.length+1;i++){
-            xor^=i;
-        }
-        return xor;
+   public int missingNumber(int[] nums) {
 
+    int xor = 0, i = 0;
+	for (i = 0; i < nums.length; i++) {
+		xor = xor ^ i ^ nums[i];
+	}
+
+	return xor ^ i;
 }
-    // public static boolean isSorted(int[] arr) {
-    //     int n = arr.length;
-    //     for (int i = 0; i < n - 1; i++) {
-    //         if (arr[i] > arr[i + 1]) {
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-      public static int findMax(int[] array) {
-        if (array == null || array.length == 0) {
-            throw new IllegalArgumentException("The array is empty.");
-        }
-
-        int maxVal = array[0];
-
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > maxVal) {
-                maxVal = array[i];
-            }
-        }
-
-        return maxVal;
-    }
 
 }
