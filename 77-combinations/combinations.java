ab06140d6df01fd3c2fb24b9356627f48190e544
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-08-01 10:24:47 +0530

    Time: 17 ms (75.30%) | Memory: 45.1 MB (84.88%) - LeetSync

---
+++ 77-combinations/combinations.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+class Solution {
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> l = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        HashSet<List<Integer>> hs = new HashSet<>();
+
+        combineHelper(n, k, 1, l, ans, hs);
+        return ans;
+    }
+
+    private void combineHelper(int n, int k, int start, List<Integer> current, List<List<Integer>> ans, HashSet<List<Integer>> hs) {
+        if (current.size() == k) {
+            List<Integer> combination = new ArrayList<>(current); // Create a copy of the current list
+            if (!hs.contains(combination)) {
+                hs.add(combination);
+                ans.add(combination);
+            }
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            current.add(i);
+            combineHelper(n, k, i + 1, current, ans, hs); // Recursively generate combinations
+            current.remove(current.size() - 1); // Backtrack by removing the last element
+        }
+    }
+}
