91082e0c3d5aebb873cdb3e1b91d861dc7b98051
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-08-01 10:30:44 +0530

    Time: 18 ms (70.85%) | Memory: 45.5 MB (51.22%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,24 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 class Solution {
 
     public List<List<Integer>> combine(int n, int k) {
-        List<Integer> currentCombination = new ArrayList<>();
+        List<Integer> l = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        
-        backtrack(1, n, k, currentCombination, ans);
-        
+        HashSet<List<Integer>> hs = new HashSet<>();
+
+        combineHelper(n, k, 1, l, ans, hs);
         return ans;
     }
-    
-    private void backtrack(int start, int n, int k, List<Integer> currentCombination, List<List<Integer>> ans) {
-        if (currentCombination.size() == k) {
-            ans.add(new ArrayList<>(currentCombination));
+
+    private void combineHelper(int n, int k, int start, List<Integer> current,List<List<Integer>> ans,             HashSet<List<Integer>> hs) {
+        if (current.size() == k) {
+            List<Integer> combination = new ArrayList<>(current); // Create a copy of the current list
+            // if (!hs.contains(combination)) {
+                hs.add(combination);
+                ans.add(combination);
+            // }
             return;
         }
-        
+
         for (int i = start; i <= n; i++) {
-            currentCombination.add(i);
-            backtrack(i + 1, n, k, currentCombination, ans);
-            currentCombination.remove(currentCombination.size() - 1);
+            current.add(i);
+            combineHelper(n, k, i + 1, current, ans, hs); // Recursively generate combinations
+            current.remove(current.size() - 1); // Backtrack by removing the last element
         }
     }
 }
