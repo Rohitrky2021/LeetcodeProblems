23ca3afe8eac87ffda5a4f7df22c7c54fb87c96c
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-08-02 11:51:20 +0530

    Time: 464 ms (25.01%) | Memory: 51.3 MB (42.87%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,37 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Sort the array to help in identifying unique triplets
+        
+        HashSet<List<Integer>> set = new HashSet<>();
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int target = -nums[i];
-                int left = i + 1;
-                int right = nums.length - 1;
-
-                while (left < right) {
-                    int sum = nums[left] + nums[right];
-
-                    if (sum == target) {
-                        ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                        // Skip duplicate elements
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-
-                        left++;
-                        right--;
-                    } else if (sum < target) {
-                        left++;
-                    } else {
-                        right--;
-                    }
+        
+        for(int i=0;i<nums.length-2;i++){
+            int s = i+1;
+            int e = nums.length-1;
+            
+            while(s < e){
+                int sum = nums[i] + nums[s] + nums[e];
+                if(sum == 0){
+                    set.add(List.of(nums[i], nums[s], nums[e]));
                 }
+                if(sum > 0) e--;
+                else s++;
             }
+            
         }
-
-        return ans;
-    }
+        return new ArrayList<>(set);
+    }    
 }
