fc3bb0c132af0c6cd6c531c31c711c29d2338c26
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-08-02 18:51:47 +0530

    Time: 28 ms (51.76%) | Memory: 44.2 MB (42.31%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,62 +1,69 @@
-class Solution {
-    public int[] getManacherValue(StringBuilder s){
-        int[] dp = new int[s.length()];
-        int i = 0, left = -1, right = -1;
-        
-        while(i < s.length()){
-            if(i > right){
-                int j = 0;
-                while(i - j >= 0 && i + j < s.length() && s.charAt(i - j) == s.charAt(i + j)){
-                    j++;
-                }
-                
-                dp[i] = j - 1;
-                left = i - dp[i];
-                right=  i + dp[i];
-            }
-            else{
-                int mirror = right + left - i;
-                int mirror_left = mirror - dp[mirror];
-                
-                if(mirror_left == left){
-                    int j = right + 1 - i;
-                    while(i - j >= 0 && i + j < s.length() && s.charAt(i - j) == s.charAt(i + j)){
-                        j++;
-                    }
+import java.util.Arrays;
 
-                    dp[i] = j - 1;
-                    left = i - dp[i];
-                    right=  i + dp[i];
-                }
-                else if(mirror_left < left){
-                    dp[i] = right - i;
-                }
-                else{
-                    dp[i] = dp[mirror];
-                }
-            }
-            i++;
+class Manacher {
+    String t;
+    int[] p;
+    int lpsStart, lpsLength;
+
+    public Manacher(String s) {
+        for (char c : s.toCharArray()) {
+            t += "#" + c;
         }
-        return dp;
+        t += "#";
+
+        build();
     }
-    
-    public String longestPalindrome(String s) {
-        StringBuilder newString = new StringBuilder("#");
-        for(int i = 0; i < s.length(); i++)
-        {
-            newString.append(s.charAt(i));
-            newString.append("#");
-        }
 
-        int[] dp = getManacherValue(newString);
-        int left = 0, right = 0;
-        
-        for(int i = 0; i < newString.length(); i++){
-            if(dp[i] *  2 >= right - left + 1){
-                left = i - dp[i];
-                right = i + dp[i];
+    void build() {
+        int n = t.length();
+        p = new int[n];
+        Arrays.fill(p, 1);
+
+        int l = 1, r = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (l + r - i >= 0)
+                p[i] = Math.max(1, Math.min(r - i, p[l + r - i]));
+
+            while (i - p[i] >= 0 && i + p[i] < n && t.charAt(i - p[i]) == t.charAt(i + p[i]))
+                p[i]++;
+
+            if (i + p[i] > r) {
+                l = i - p[i];
+                r = i + p[i];
+
+                if (lpsLength < p[i]) {
+                    lpsStart = l + 2;
+                    lpsLength = p[i] - 1;
+                }
             }
         }
-        return newString.substring(left, right + 1).replace("#", "");
+    }
+
+    int getLongest(int center, boolean odd) {
+        int position = 2 * center + 1 + (odd ? 1 : 0);
+        return p[position] - 1;
+    }
+
+    boolean checkPalindrome(int l, int r) {
+        if ((r - l + 1) <= getLongest((l + r) / 2, (r - l + 1) == 1))
+            return true;
+        return false;
+    }
+
+    String longestPalindromeSubstring() {
+        int n = t.length();
+        StringBuilder ans = new StringBuilder();
+        for (int i = lpsStart; i < n && lpsLength-- > 0; i += 2) {
+            ans.append(t.charAt(i));
+        }
+        return ans.toString();
+    }
+}
+
+class Solution {
+    public String longestPalindrome(String s) {
+        Manacher m = new Manacher(s);
+        return m.longestPalindromeSubstring();
     }
 }
