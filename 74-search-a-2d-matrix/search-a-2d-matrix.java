c779b3e64348c85bb614b89b1b3d6573e314d08e
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-08-07 17:49:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (96.14%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -8,22 +8,50 @@ class Solution {
     //    while(i<)
     // }
 
-    public boolean searchMatrix(int[][] m, int t) {
-        int i=0,j=m[0].length-1;
+    // public boolean searchMatrix(int[][] m, int t) {
+    //     int i=0,j=m[0].length-1;
 
-        while(i<m.length && j>=0){
-            if(m[i][j]==t){
-                return true;
-            }
-            if(m[i][j]<t){
-                i++;
-            }
-            else if(m[i][j]>t){
-                j--;
-            }
-        }
-        return false;
+    //     while(i<m.length && j>=0){
+    //         if(m[i][j]==t){
+    //             return true;
+    //         }
+    //         if(m[i][j]<t){
+    //             i++;
+    //         }
+    //         else if(m[i][j]>t){
+    //             j--;
+    //         }
+    //     }
+    //     return false;
 
-    }
+    // }
+    
+   /**
+ *  Do binary search in this "ordered" matrix
+ */
+public boolean searchMatrix(int[][] matrix, int target) {
+	
+	int row_num = matrix.length;
+	int col_num = matrix[0].length;
+	
+	int begin = 0, end = row_num * col_num - 1;
+	if (matrix.length == 0 || matrix[0].length == 0) return false;
+	while(begin <= end){
+		int mid = begin + (end-begin) / 2;
+		int mid_value = matrix[mid/col_num][mid%col_num];
+		
+		if( mid_value == target){
+			return true;
+		
+		}else if(mid_value < target){
+			//Should move a bit further, otherwise dead loop.
+			begin = mid+1;
+		}else{
+			end = mid-1;
+		}
+	}
+	
+	return false;
+}
        
 }
