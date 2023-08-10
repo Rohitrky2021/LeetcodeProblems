cd424c24e7d4122e4456c85b2e351dac9d25b344
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-08-10 09:21:46 +0530

    Time: 13 ms (7.14%) | Memory: 42 MB (99.63%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -6,19 +6,31 @@ class Solution {
 
         int high = n - 1;
         while (low <= high) {
-            while (low < high && nums[low] == nums[low + 1]) ++low;
-            while (low < high && nums[high] == nums[high - 1]) --high;
+            // same thing Done in 3 SUM check that out also if the num are Duplicate
+            // while (low < high && nums[low] == nums[low + 1]) ++low;
+            // while (low < high && nums[high] == nums[high - 1]) --high;
+            // ===========================================================
+            // while (low <= high && nums[low] == nums[high]) {
+            //     if (nums[low] == target) {
+            //         return true;
+            //     }
+            //     low++;
+            //     high--;
+            // }
             int mid = low + (high - low) / 2;
             System.out.println(mid + " ");
             if (target == nums[mid]) return true;
 
             //    l1
-            if (nums[low] <= nums[mid]) {
+            if (nums[low] < nums[mid]) {
                 if (nums[low] <= target && target < nums[mid]) high = mid - 1; else low = mid + 1;
             }
             //l2
-            else {
+            else if(nums[low]> nums[mid]){
                 if (nums[mid] < target && target <= nums[high]) low = mid + 1; else high = mid - 1;
+            }
+            else{
+                low+=1;
             }
         }
         System.gc();
