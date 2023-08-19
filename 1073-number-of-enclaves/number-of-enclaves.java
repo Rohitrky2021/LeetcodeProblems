eed9a4a3030b0b90306bb82ca556138317bd7012
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-08-19 11:08:40 +0530

    Time: 9 ms (73.23%) | Memory: 55.3 MB (40.37%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -2,36 +2,46 @@ class Solution {
 
     // Trick -->Reverse Thinking All 4 Boundries k 1 ko Check dfs Krke Jitne Connected Honge Unko Zero Krdenge
     // those 1's who are left are not connected to any Boundary
-   public int numEnclaves(int[][] grid) {
-    int count = 0;
-    for (int i = 0; i < grid.length; i++) {
-        for (int j = 0; j < grid[0].length; j++) { // Iterate over all columns as well
-            if (i == 0 || j == 0 || i == grid.length - 1 || j == grid[0].length - 1) {
-                dfs(grid, i, j);
+    public int numEnclaves(int[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) { // Iterate over all columns as well
+                if (i == 0 || j == 0 || i == grid.length - 1 || j == grid[0].length - 1) {
+                    dfs(grid, i, j);
+                }
             }
         }
-    }
-    for (int i = 0; i < grid.length; i++) {
-        for (int j = 0; j < grid[0].length; j++) { // Iterate over all columns as well
-            if (grid[i][j] == 1) {
-                count++;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) { // Iterate over all columns as well
+                if (grid[i][j] == 1) {
+                    count++;
+                }
             }
         }
+        return count;
     }
-    return count;
-}
-
     public void dfs(int[][] grid, int i, int j) {
-            if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j]==0) {
-                return;
-            }
-    grid[i][j]=0;
-        dfs(grid, i - 1, j);
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j - 1);
-        dfs(grid, i, j + 1);
-
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
+        return;
     }
+    grid[i][j] = 0;
+
+    for (int[] dir : direc) {
+        int newRow = i + dir[0];
+        int newCol = j + dir[1];
+        dfs(grid, newRow, newCol);
+    }
+}
+    // public void dfs(int[][] grid, int i, int j) {
+    //     if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
+    //         return;
+    //     }
+    //     grid[i][j] = 0;
+    //     dfs(grid, i - 1, j);
+    //     dfs(grid, i + 1, j);
+    //     dfs(grid, i, j - 1);
+    //     dfs(grid, i, j + 1);
+    // }
 
     static int[][] direc = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 }
