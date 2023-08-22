b276a8d409bd9dbf1309c27bcb29b92c270c4615
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-08-22 15:42:57 +0530

    Time: 20 ms (16.76%) | Memory: 43.7 MB (44.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,60 +1,60 @@
-// class Solution {
-
-//     public boolean isAnagram(String s, String s2) {
-//         if (s.length() != s2.length()) {
-//             return false;
-//         }
-//         HashMap<Character, Integer> map = new HashMap<>();
-
-//         for (int i = 0; i < s.length(); i++) {
-//             if (map.containsKey(s.charAt(i))) {
-//                 map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
-//             } else {
-//                 map.put(s.charAt(i), 1);
-//             }
-//         }
-
-//         for (int i = 0; i < s2.length(); i++) {
-//             if (map.containsKey(s2.charAt(i))) {
-//                 map.put(s2.charAt(i), map.get(s2.charAt(i)) - 1);
-            // if (map.get(s2.charAt(i)) == 0) {
-            //     frequencyMap.remove(s2.charAt(i));
-            // }
-//             } else {
-//                 return false;
-//             }
-//         }
-
-//         return map.isEmpty();
-//     }
-// }
-import java.util.HashMap;
-
 class Solution {
 
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+    public boolean isAnagram(String s, String s2) {
+        if (s.length() != s2.length()) {
             return false;
         }
-        
-        HashMap<Character, Integer> frequencyMap = new HashMap<>();
-        
-        // Count characters in string s
-        for (char c : s.toCharArray()) {
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
         }
-        
-        // Compare character frequencies with string t
-        for (char c : t.toCharArray()) {
-            if (!frequencyMap.containsKey(c)) {
+
+        for (int i = 0; i < s2.length(); i++) {
+            if (map.containsKey(s2.charAt(i))) {
+                map.put(s2.charAt(i), map.get(s2.charAt(i)) - 1);
+            if (map.get(s2.charAt(i)) == 0) {
+                map.remove(s2.charAt(i));
+            }
+            } else {
                 return false;
             }
-            frequencyMap.put(c, frequencyMap.get(c) - 1);
-            if (frequencyMap.get(c) == 0) {
-                frequencyMap.remove(c);
-            }
         }
-        
-        return frequencyMap.isEmpty();
+
+        return map.isEmpty();
     }
 }
+// import java.util.HashMap;
+
+// class Solution {
+
+//     public boolean isAnagram(String s, String t) {
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+        
+//         HashMap<Character, Integer> frequencyMap = new HashMap<>();
+        
+//         // Count characters in string s
+//         for (char c : s.toCharArray()) {
+//             frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+//         }
+        
+//         // Compare character frequencies with string t
+//         for (char c : t.toCharArray()) {
+//             if (!frequencyMap.containsKey(c)) {
+//                 return false;
+//             }
+//             frequencyMap.put(c, frequencyMap.get(c) - 1);
+//             if (frequencyMap.get(c) == 0) {
+//                 frequencyMap.remove(c);
+//             }
+//         }
+        
+//         return frequencyMap.isEmpty();
+//     }
+// }
