d2996667080b57e32fec5b8fef0e1787d30c4972
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-08-22 15:07:47 +0530

    Time: 10 ms (88.71%) | Memory: 54.7 MB (93.13%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true; // Found a duplicate
+            }
+            seen.add(num); // Add the number to the HashSet
+        }
+        
+        return false; // No duplicates found
+    }
+}
