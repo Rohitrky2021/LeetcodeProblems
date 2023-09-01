f030c1d2a8bde4a659ae28440cd8c5353109deef
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-01 10:07:50 +0530

    Time: 1 ms (94.03%) | Memory: 43.6 MB (90.61%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,32 +1,64 @@
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         List<Integer> current = new ArrayList<>();
+//         boolean[] visited = new boolean[nums.length];
+//         backtrack(nums, visited, current, result);
+//         return result;
+//     }
+
+//     private void backtrack(int[] nums, boolean[] visited, List<Integer> current, List<List<Integer>> result) {
+//         if (current.size() == nums.length) {
+//             result.add(new ArrayList<>(current));
+//             return;
+//         }
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (visited[i]) {
+//                 continue;
+//             }
+//             //  if (visited[i]) {
+//             //     continue;
+//             //     // ---------------->when revise ones uncomment and run this !
+//             // }
+//             visited[i] = true;
+//             current.add(nums[i]);
+//             backtrack(nums, visited, current, result);
+//             current.remove(current.size() - 1);
+//             visited[i] = false;
+//         }
+//     }
+// }
+
+// M2
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
-        backtrack(nums, visited, current, result);
+        backtrack(nums, 0, current, result);
         return result;
     }
 
-    private void backtrack(int[] nums, boolean[] visited, List<Integer> current, List<List<Integer>> result) {
-        if (current.size() == nums.length) {
+    private void backtrack(int[] nums, int start, List<Integer> current, List<List<Integer>> result) {
+        if (start == nums.length) {
             result.add(new ArrayList<>(current));
             return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            //  if (visited[i]) {
-            //     continue;
-            //     // ---------------->when revise ones uncomment and run this !
-            // }
-            visited[i] = true;
-            current.add(nums[i]);
-            backtrack(nums, visited, current, result);
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i); // Swap elements to generate permutations
+            current.add(nums[start]);
+            backtrack(nums, start + 1, current, result);
             current.remove(current.size() - 1);
-            visited[i] = false;
+            swap(nums, start, i); // Swap back to backtrack properly
         }
     }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
+
 
