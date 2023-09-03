55c92029fa7538c8c98a1e465f4ccdcb842173a2
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-03 13:37:18 +0530

    Time: 1 ms (97.80%) | Memory: 43.9 MB (46.88%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -39,11 +39,11 @@ class Solution {
         dp = new int[rows][cols];
 
         // Initialize the DP table with maximum values
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                dp[i][j] = -1;
-            }
-        }
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < cols; j++) {
+        //         dp[i][j] = -1;
+        //     }
+        // }
 
         return helper(grid, 0, 0);
     }
@@ -57,7 +57,7 @@ class Solution {
             return grid[r][c]; // Return the value at the destination
         }
 
-        if (dp[r][c] != -1) {
+        if (dp[r][c] >0) {
             return dp[r][c]; // If the value is already calculated, return it
         }
 
