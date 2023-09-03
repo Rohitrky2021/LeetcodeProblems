050791411781ee3b09ddac2f2db8fed6e267ee00
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-03 13:36:16 +0530

    Time: 1 ms (97.80%) | Memory: 43.9 MB (58.58%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,31 +1,74 @@
+// class Solution {
+//     int[][] dp;
+//     int mod = 1_000_000_007;
+
+//     public int minPathSum(int[][] grid) {
+//         int rows = grid.length;
+//         int cols = grid[0].length;
+//         dp = new int[rows][cols];
+
+//         return helper(grid, 0, 0, 0, Integer.MAX_VALUE);
+//     }
+
+//     int helper(int[][] grid, int r, int c, int sum, int k) {
+//         if (r == grid.length || c == grid[0].length) {
+//             return Integer.MAX_VALUE;
+//         }
+
+//         sum += grid[r][c];
+//         if (r == grid.length - 1 && c == grid[0].length - 1) { // r==m-1 && c==n-1
+//         System.out.println(sum+" "+grid[r][c]);
+//             return grid[r][c];    
+//         }
+
+//         // For this Think iun 3D i.e -> for every r,c their will be List of sum%k values
+//         if (dp[r][c] > 0) {
+//             return dp[r][c]; // i.r eske pehle ka Remainder i have handleded end result same hoga as Sum of + This Remainder
+//         }
+
+//         return dp[r][c] =  grid[r][c] +Math.min(helper(grid, r + 1, c, sum, k) , helper(grid, r, c + 1, sum, k) ); 
+//     }
+// }
+
 class Solution {
     int[][] dp;
-    int mod = 1_000_000_007;
 
     public int minPathSum(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
         dp = new int[rows][cols];
 
-        return helper(grid, 0, 0, 0, Integer.MAX_VALUE);
+        // Initialize the DP table with maximum values
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return helper(grid, 0, 0);
     }
 
-    int helper(int[][] grid, int r, int c, int sum, int k) {
+    int helper(int[][] grid, int r, int c) {
         if (r == grid.length || c == grid[0].length) {
-            return Integer.MAX_VALUE;
+            return Integer.MAX_VALUE; // Return a very large value for out-of-bounds cases
         }
 
-        sum += grid[r][c];
-        if (r == grid.length - 1 && c == grid[0].length - 1) { // r==m-1 && c==n-1
-        System.out.println(sum+" "+grid[r][c]);
-            return grid[r][c];    
+        if (r == grid.length - 1 && c == grid[0].length - 1) {
+            return grid[r][c]; // Return the value at the destination
         }
 
-        // For this Think iun 3D i.e -> for every r,c their will be List of sum%k values
-        if (dp[r][c] > 0) {
-            return dp[r][c]; // i.r eske pehle ka Remainder i have handleded end result same hoga as Sum of + This Remainder
+        if (dp[r][c] != -1) {
+            return dp[r][c]; // If the value is already calculated, return it
         }
 
-        return dp[r][c] =  grid[r][c] +Math.min(helper(grid, r + 1, c, sum, k) , helper(grid, r, c + 1, sum, k) ); 
+        // Calculate the minimum path sum by moving down and right
+        int right = helper(grid, r, c + 1);
+        int down = helper(grid, r + 1, c);
+
+        // Update the DP table with the minimum path sum at this cell
+        dp[r][c] = grid[r][c] + Math.min(right, down);
+
+        return dp[r][c];
     }
 }
+
