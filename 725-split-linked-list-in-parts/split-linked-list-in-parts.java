33e79bd6959c95f95b5e35605fb76683953a8b50
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-06 19:46:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (83.86%) - LeetSync

---
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.java
@@ -0,0 +1,38 @@
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int size = 0;
+        ListNode s1 = head;
+
+        while (s1 != null) {
+            s1 = s1.next;
+            size++;
+        }
+
+        int partSize = size / k;
+        int remainder = size % k;
+
+        ListNode[] ans = new ListNode[k];
+        ListNode current = head;
+        int i = 0;
+
+        while (i < k) {
+            ans[i] = current;
+
+            int partNodeCount = partSize + (i < remainder ? 1 : 0);
+
+            for (int j = 1; j < partNodeCount; j++) {
+                current = current.next;
+            }
+            if (current != null) {
+            ListNode nextNode = current.next;
+            current.next = null;
+            
+            // Move the current pointer to the next node
+            current = nextNode;
+            }
+            i++;
+        }
+
+        return ans;
+    }
+}
