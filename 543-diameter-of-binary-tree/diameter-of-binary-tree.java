119ce286402adbc9753969eac10c5a4e143f546c
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-06 22:56:02 +0530

    Time: 1 ms (25.73%) | Memory: 42.9 MB (9.59%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,35 +13,66 @@
  *     }
  * }
  */
+
 class Solution {
     int diameter = 0;
 
+    Map<TreeNode, Integer> memo = new HashMap<>();
+
     public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null) {
+        height(root);
+        return diameter - 1;
+    }
+
+    public int height(TreeNode node) {
+        if (node == null) {
             return 0;
         }
 
-        int[] result = diameterHelper(root);
-        return Math.max(result[0], result[1]) - 1;
-    }
-
-    public int[] diameterHelper(TreeNode node) {
-        if (node == null) {
-            return new int[] { 0, 0 };
+        if (memo.containsKey(node)) {
+            return memo.get(node);
         }
 
-        int[] left = diameterHelper(node.left);
-        int[] right = diameterHelper(node.right);
+        int lh = height(node.left);
+        int rh = height(node.right);
 
-        int maxPathThroughRoot = left[0] + right[0] + 1;
-        int maxDiameterWithoutRoot = Math.max(left[1], right[1]);
+        int dia = lh + rh + 1;
+        diameter = Math.max(diameter, dia);
 
-        int currentMaxDiameter = Math.max(maxPathThroughRoot, maxDiameterWithoutRoot);
+        int maxHeight = Math.max(lh, rh) + 1;
+        memo.put(node, maxHeight);
 
-        int currentMaxPath = Math.max(left[0], right[0]) + 1;
-
-        return new int[] { currentMaxPath, currentMaxDiameter };
+        return maxHeight;
     }
+
+    
+
+    // public int diameterOfBinaryTree(TreeNode root) {
+    //     if (root == null) {
+    //         return 0;
+    //     }
+
+    //     int[] result = diameterHelper(root);
+    //     return Math.max(result[0], result[1]) - 1;
+    // }
+
+    // public int[] diameterHelper(TreeNode node) {
+    //     if (node == null) {
+    //         return new int[] { 0, 0 };
+    //     }
+
+    //     int[] left = diameterHelper(node.left);
+    //     int[] right = diameterHelper(node.right);
+
+    //     int maxPathThroughRoot = left[0] + right[0] + 1;
+    //     int maxDiameterWithoutRoot = Math.max(left[1], right[1]);
+
+    //     int currentMaxDiameter = Math.max(maxPathThroughRoot, maxDiameterWithoutRoot);
+
+    //     int currentMaxPath = Math.max(left[0], right[0]) + 1;
+
+    //     return new int[] { currentMaxPath, currentMaxDiameter };
+    // }
 
     // public int diameterOfBinaryTree(TreeNode root) {
     //     Map<TreeNode, Integer> memo = new HashMap<>();
@@ -69,8 +100,6 @@ class Solution {
     //     return height;
     // }
 
-
-    
     // public int diameterOfBinaryTree(TreeNode root) {
     //     height(root);
     //     return diameter - 1;
