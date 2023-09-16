faddc22401a9b612cfa4604598a5522fce02ce8b
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-17 00:59:39 +0530

    Time: 5 ms (33.33%) | Memory: 58.4 MB (66.67%) - LeetSync

--- 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.java
+++ 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.java
@@ -12,3 +12,24 @@
       return ans;
       }
    }
+
+//    class Solution {
+//     public int minLengthAfterRemovals(List<Integer> nums) {
+//         int n = nums.size();
+//         if (n==1) return 1;
+        
+//         int d = n/2, ans = 0, j=d;
+//         if (n%2!=0) {
+//             ans = ans + 1;
+//             j = d+1;
+//         }
+
+//         //i for the first half and j for the second half
+//         for (int i=0; i<d; i++) {
+//             if ((int)nums.get(i) == (int)nums.get(j)) ans = ans +2;
+//             j++;
+//         }
+        
+//         return ans;     
+//     }
+// }
