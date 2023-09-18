9310716b950b7fcc9711cb685783a61b11d5a25b
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-18 23:23:12 +0530

    Time: 2520 ms (28.84%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -3,16 +3,13 @@
 # FROM Sales s
 # JOIN Product p ON s.product_id = p.product_id;
 
-
 SELECT 
-     p.product_name, s.year, s.price
+    DISTINCT p.product_name, s.year, s.price
 FROM 
     Sales s
 JOIN 
     Product p
 ON
     s.product_id = p.product_id
-
-
 
  
