697ffb2b52780530ba8f4e172d6014d67b73304d
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-23 01:29:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (66.95%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,15 +1,54 @@
+// class Solution {
+
+//    public boolean isSubsequence(String s, String t) {
+//     int sIndex = 0;
+    
+//     for (int tIndex = 0; tIndex < t.length() && sIndex < s.length(); tIndex++) {
+//         if (s.charAt(sIndex) == t.charAt(tIndex)) {
+//             sIndex++;
+//         }
+//     }
+    
+//     return sIndex == s.length();
+// }
+
+// }
+
 class Solution {
+    public boolean isSubsequence(String s, String t) {
+        // 1 ms code beat 53% 
+        // int si=0;
+        // if(s.length()<1)
+        //     return true;
+        // for(int i=0;i<t.length();i++){
+        //     if(s.charAt(si)==t.charAt(i))
+        //         si++;
 
-   public boolean isSubsequence(String s, String t) {
-    int sIndex = 0;
-    
-    for (int tIndex = 0; tIndex < t.length() && sIndex < s.length(); tIndex++) {
-        if (s.charAt(sIndex) == t.charAt(tIndex)) {
-            sIndex++;
+        //     if(si==s.length())
+        //         return true;
+        // }
+
+
+    // 1 ms beat 100
+    int i=0,j=0;
+    int n=t.length();
+    int m=s.length();
+    char ss[]=s.toCharArray();
+    char tt[]=t.toCharArray();
+
+    if(m<1)
+        return true;
+
+    while(i<n){
+        if(tt[i]==ss[j]){
+            j++;
         }
+        i++;
+        
+    if(j==m)
+        return true;
     }
-    
-    return sIndex == s.length();
-}
 
+        return false;
+    }
 }
