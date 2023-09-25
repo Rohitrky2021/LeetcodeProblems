5d66d047ce61af9209e3f4e05d70592a0e9fa027
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-25 11:17:04 +0530

    Time: 12 ms (10.41%) | Memory: 40.8 MB (42.11%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,15 +1,30 @@
 class Solution {
 
-    public char findTheDifference(String s, String t) {
-        char a1[] = s.toCharArray();
-        char a2[] = t.toCharArray();
+    // public char findTheDifference(String s, String t) {
+    //    char a1[]=s.toCharArray();
+    //     char a2[]=t.toCharArray();
 
-        Arrays.sort(a1);
-        Arrays.sort(a2);
-        for (int i = 0; i < a1.length; i++) {
-            if (a1[i] != a2[i]) return a2[i];
+    //     Arrays.sort(a1);
+    //     Arrays.sort(a2);
+    //     for (int i = 0; i < a1.length; i++) {
+    //         if(a1[i]!=a2[i])return a2[i];
+    //     }
+
+    //       return a2[s.length()];
+    // }
+
+    public char findTheDifference(String s, String t) {
+        Map<Character, Integer> hs = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            hs.put(c, hs.getOrDefault(c, 0) + 1);
+        }
+        for (char i : t.toCharArray()) {
+            if ( hs.get(i) == null || hs.get(i) == 0 ) return i;
+            if (hs.containsKey(i)) {
+                hs.put(i, hs.getOrDefault(i, 0) - 1);
+            }
         }
 
-        return a2[s.length()];
+        return 'k';
     }
 }
