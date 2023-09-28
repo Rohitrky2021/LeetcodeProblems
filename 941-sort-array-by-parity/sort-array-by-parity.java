55da1e8a66da61c50f68a1f2f258cdc85752138f
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-28 19:01:59 +0530

    Time: 1 ms (81.28%) | Memory: 44.3 MB (33.59%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -24,41 +24,41 @@ class Solution {
     // }
 
 // M1.2--> Optimised for Space for the above one 
-    public int[] sortArrayByParity(int[] nums) {
-        int i = 0, j = 0;
-
-        while (i < nums.length) {
-            if (nums[i] % 2 == 0) {
-                // Swap the even number at position i with the number at position j
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-                j++;
-                continue;
-            }
-            i++;
-        }
-
-        return nums;
-    }
-
-    // M2 --> Wihtout Swaping
     // public int[] sortArrayByParity(int[] nums) {
-    //     int[] result = new int[nums.length];
-    //     int evenIndex = 0;
-    //     int oddIndex = nums.length - 1;
+    //     int i = 0, j = 0;
 
-    //     for (int num : nums) {
-    //         if (num % 2 == 0) {
-    //             result[evenIndex] = num;
-    //             evenIndex++;
-    //         } else {
-    //             result[oddIndex] = num;
-    //             oddIndex--;
+    //     while (i < nums.length) {
+    //         if (nums[i] % 2 == 0) {
+    //             // Swap the even number at position i with the number at position j
+    //             int temp = nums[i];
+    //             nums[i] = nums[j];
+    //             nums[j] = temp;
+    //             i++;
+    //             j++;
+    //             continue;
     //         }
+    //         i++;
     //     }
 
-    //     return result;
+    //     return nums;
     // }
+
+    // M2 --> Wihtout Swaping
+    public int[] sortArrayByParity(int[] nums) {
+        int[] result = new int[nums.length];
+        int evenIndex = 0;
+        int oddIndex = nums.length - 1;
+
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                result[evenIndex] = num;
+                evenIndex++;
+            } else {
+                result[oddIndex] = num;
+                oddIndex--;
+            }
+        }
+
+        return result;
+    }
 }
