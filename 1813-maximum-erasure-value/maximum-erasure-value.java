ae5e15b5b408243a8d31cef78e50184bb07f5f71
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-29 00:32:14 +0530

    Time: 92 ms (8.82%) | Memory: 55.1 MB (98.27%) - LeetSync

---
+++ 1813-maximum-erasure-value/maximum-erasure-value.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    // main hook of COnitnuos is ==>  exactly one subarray.  &&  unique elements-->tell us to use hashmap for freq
+
+    public int maximumUniqueSubarray(int[] arr) { //using video template
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        int tail = 0, ans = 0, sum = 0, head/*makeing it global */;
+
+        for (head = 0; head < arr.length; head++) {
+            hm.put(arr[head], hm.getOrDefault(arr[head], 0) + 1);
+
+            while (hm.getOrDefault(arr[head], 0) > 1) {
+                ans = Math.max(ans, sum);
+                sum = sum - arr[tail];
+                hm.put(arr[tail], hm.getOrDefault(arr[tail], 0) - 1);
+                tail++;
+            }
+
+            if(hm.getOrDefault(arr[head], 0)==1)sum += arr[head];
+            
+            // ans = Math.max(ans, sum);
+        }
+
+        // return head-tail+1;
+        return  Math.max(ans, sum);
+    }
+}
