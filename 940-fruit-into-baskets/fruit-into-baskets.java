d5fd57994e2829e4b96889c0892a0a58fd8a00a2
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-29 12:56:29 +0530

    Time: 6 ms (94.81%) | Memory: 49.1 MB (98.82%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -23,22 +23,54 @@
 //     }
 // }
 
+// class Solution {
+
+//     public int totalFruit(int[] fruits) {
+//         Map<Integer, Integer> fruitCount = new HashMap<>();
+//         int tail = 0, ans = 0;
+
+//         for (int head = 0; head < fruits.length; head++) {
+//             int currentFruit = fruits[head];
+//             fruitCount.put(currentFruit, fruitCount.getOrDefault(currentFruit, 0) + 1);
+
+//             // Check if the window needs adjustments to have at most two types of fruit
+//             while (fruitCount.size() > 2 && tail<fruits.length) {
+//                 int leftFruit = fruits[tail];
+//                 fruitCount.put(leftFruit, fruitCount.getOrDefault(leftFruit,0) - 1); // fr[tail]-
+//                 if (fruitCount.get(leftFruit) == 0) {// remove krna pdega or wrong ans 
+//                     fruitCount.remove(leftFruit);
+//                 }
+//                 tail++;
+//             }
+
+//             ans = Math.max(ans, head - tail + 1);
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
 
     public int totalFruit(int[] fruits) {
-        Map<Integer, Integer> fruitCount = new HashMap<>();
-        int tail = 0, ans = 0;
+        int[] fruitCount = new int[fruits.length];
+        int tail = 0, ans = 0, distinctFruits = 0;
 
         for (int head = 0; head < fruits.length; head++) {
             int currentFruit = fruits[head];
-            fruitCount.put(currentFruit, fruitCount.getOrDefault(currentFruit, 0) + 1);
+            
+            if (fruitCount[currentFruit] == 0) {
+                distinctFruits++;
+            }
+            
+            fruitCount[currentFruit]++;
 
             // Check if the window needs adjustments to have at most two types of fruit
-            while (fruitCount.size() > 2 && tail<fruits.length) {
+            while (distinctFruits > 2 && tail < fruits.length) {
                 int leftFruit = fruits[tail];
-                fruitCount.put(leftFruit, fruitCount.getOrDefault(leftFruit,0) - 1); // fr[tail]-
-                if (fruitCount.get(leftFruit) == 0) {// remove krna pdega or wrong ans 
-                    fruitCount.remove(leftFruit);
+                fruitCount[leftFruit]--;
+                if (fruitCount[leftFruit] == 0) {
+                    distinctFruits--;
                 }
                 tail++;
             }
@@ -49,4 +81,5 @@ class Solution {
         return ans;
     }
 }
+
 
