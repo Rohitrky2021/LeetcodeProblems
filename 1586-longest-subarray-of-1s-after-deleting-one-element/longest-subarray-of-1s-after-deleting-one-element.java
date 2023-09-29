0f6842a80d66f7832fc0163d349ad16ce290e8cc
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-09-29 10:32:10 +0530

    Time: 3 ms (43.83%) | Memory: 55.1 MB (92.11%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -30,7 +30,7 @@ class Solution {
         int tail = 0, ans = 0, sum = 0,   k=1, head/*makeing it global */;
         for (head = 0; head < arr.length; head++) {
             if(arr[head]==0)k--;
-            ans=Math.max(ans,head-tail);
+            // ans=Math.max(ans,head-tail);
 
             
             while (k<0) {
