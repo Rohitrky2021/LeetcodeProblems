d97dbd9009625ca40f3b7bcdf5c9f713836a3668
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-10-02 04:38:27 +0530

    Time: 1 ms (99.85%) | Memory: 48.4 MB (53.47%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,27 +1,47 @@
-class Solution {
+// class Solution {
 
-    public int majorityElement(int[] arr) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+    
 
-        Set<Integer> keyset=map.keySet();
-        for (int i = 0; i < arr.length; i++) {
-            if(map.containsKey(arr[i])){
-                map.put(arr[i], map.get(arr[i])+1);
-            }
-            else{
-                map.put(arr[i], 1);
-            }
+//     public int majorityElement(int[] arr) {
+//         HashMap<Integer,Integer> map=new HashMap<>();
+
+//         Set<Integer> keyset=map.keySet();
+//         for (int i = 0; i < arr.length; i++) {
+//             if(map.containsKey(arr[i])){
+//                 map.put(arr[i], map.get(arr[i])+1);
+//             }
+//             else{
+//                 map.put(arr[i], 1);
+//             }
             
 
-        }
+//         }
 
         
-        for(Integer key:keyset){
-            if(map.get(key)>arr.length/2){
-                System.out.println(key.intValue());
-                return key.intValue(); 
-            }
+//         for(Integer key:keyset){
+//             if(map.get(key)>arr.length/2){
+//                 System.out.println(key.intValue());
+//                 return key.intValue(); 
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+public class Solution {
+    public int majorityElement(int[] num) {
+
+        int major=num[0], count = 1;
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+            
         }
-        return -1;
+        return major;
     }
 }
