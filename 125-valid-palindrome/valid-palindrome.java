4da9cd7ee9aa42e23db969f902bffe7b0ec0016f
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-10-01 13:52:16 +0530

    Time: 4 ms (57.84%) | Memory: 44 MB (26.13%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -15,19 +15,46 @@
 //     }
 // }
 
+// class Solution {
+//     public boolean isPalindrome(String xyx) {
+//         int n = xyx.length();
+//         xyx = xyx.toLowerCase();
+//         xyx = xyx.replaceAll("[^a-zA-Z0-9]", "");
+//         // if(n!=xyx.length() && xyx.length()==1)return false;
+//           n = xyx.length();
+          
+//         for (int i = 0; i < n / 2; i++) {
+//             if (xyx.charAt(i) != xyx.charAt(n - 1 - i)) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
 class Solution {
     public boolean isPalindrome(String xyx) {
         int n = xyx.length();
         xyx = xyx.toLowerCase();
-        xyx = xyx.replaceAll("[^a-zA-Z0-9]", "");
-        // if(n!=xyx.length() && xyx.length()==1)return false;
-          n = xyx.length();
-          
+
+        StringBuilder cleanString = new StringBuilder();
+        
+        for (int i = 0; i < n; i++) {
+            char c = xyx.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                cleanString.append(c);
+            }
+        }
+
+        n = cleanString.length();
+
         for (int i = 0; i < n / 2; i++) {
-            if (xyx.charAt(i) != xyx.charAt(n - 1 - i)) {
+            if (cleanString.charAt(i) != cleanString.charAt(n - 1 - i)) {
                 return false;
             }
         }
+        
         return true;
     }
 }
