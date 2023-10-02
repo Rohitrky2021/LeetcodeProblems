5a1b4e66b96842a5b8bbd80cf3e23759d4ce2131
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-10-02 21:20:56 +0530

    Time: 1 ms (89.63%) | Memory: 42.2 MB (77.97%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,21 +1,47 @@
-class Solution {
+// class Solution {
 
+//     public List<String> generateParenthesis(int n) {
+//         List<String> ans = new ArrayList<>();
+//         helper(0, n, ans, 0, "");
+//         return ans;
+//     }
+
+//     public List<String> helper(int close, int n, List<String> ans, int open, String s) {
+//         if (open == n && close == n) {
+//             ans.add(s);
+//             return ans;
+//         }
+
+//         if (open < n) helper(close, n, ans, open + 1, s + "(");
+
+//         if (close < open) helper(close + 1, n, ans, open, s + ")"); // If condition is important here
+
+//         return ans;
+//     }
+// }
+class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        helper(0, n, ans, 0, "");
+        generateParenthesisDP(n, n, "", ans, new StringBuilder());
         return ans;
     }
 
-    public List<String> helper(int close, int n, List<String> ans, int open, String s) {
-        if (open == n && close == n) {
-            ans.add(s);
-            return ans;
+    public void generateParenthesisDP(int open, int close, String current, List<String> ans, StringBuilder temp) {
+        if (open == 0 && close == 0) {
+            ans.add(current);
+            return;
         }
 
-        if (open < n) helper(close, n, ans, open + 1, s + "(");
+        if (open > 0) {
+            temp.append('(');
+            generateParenthesisDP(open - 1, close, current + '(', ans, temp);
+            temp.deleteCharAt(temp.length() - 1);
+        }
 
-        if (close < open) helper(close + 1, n, ans, open, s + ")");
-
-        return ans;
+        if (close > open) {
+            temp.append(')');
+            generateParenthesisDP(open, close - 1, current + ')', ans, temp);
+            temp.deleteCharAt(temp.length() - 1);
+        }
     }
 }
