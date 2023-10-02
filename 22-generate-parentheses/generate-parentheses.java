3538b2b62cc5b519abfdc89d08a71c70918112ee
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-10-03 02:03:41 +0530

    Time: 1 ms (89.63%) | Memory: 41.7 MB (92.87%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -21,31 +21,56 @@
 //     }
 // }
  
+class Solution {
+  public List<String> generateParenthesis(int n) {
+    List<String> ans = new ArrayList<>();
+    dfs(n, n, new StringBuilder(), ans);
+    return ans;
+  }
+
+  private void dfs(int l, int r, final StringBuilder sb, List<String> ans) {
+    if (l == 0 && r == 0) {
+      ans.add(sb.toString());
+      return;
+    }
+
+    if (l > 0) {
+      sb.append("(");
+      dfs(l - 1, r, sb, ans);
+      sb.deleteCharAt(sb.length() - 1);
+    }
+    if (l < r) {
+      sb.append(")");
+      dfs(l, r - 1, sb, ans);
+      sb.deleteCharAt(sb.length() - 1);
+    }
+  }
+}
 
 // Now Using Tabulation DP 
 
-class Solution {
+// class Solution {
 
-    public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<>();
-        List<List<String>> dp = new ArrayList<>();
+//     public List<String> generateParenthesis(int n) {
+//         List<String> ans = new ArrayList<>();
+//         List<List<String>> dp = new ArrayList<>();
         
-        for (int i = 0; i <= n; i++) {
-            dp.add(new ArrayList<String>());
-        }
+//         for (int i = 0; i <= n; i++) {
+//             dp.add(new ArrayList<String>());
+//         }
         
-        dp.get(0).add("");
+//         dp.get(0).add("");
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                for (String str1 : dp.get(j)) {
-                    for (String str2 : dp.get(i - j - 1)) {
-                        dp.get(i).add("(" + str1 + ")" + str2);
-                    }
-                }
-            }
-        }
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 0; j < i; j++) {
+//                 for (String str1 : dp.get(j)) {
+//                     for (String str2 : dp.get(i - j - 1)) {
+//                         dp.get(i).add("(" + str1 + ")" + str2);
+//                     }
+//                 }
+//             }
+//         }
         
-        return dp.get(n);
-    }
-}
+//         return dp.get(n);
+//     }
+// }
