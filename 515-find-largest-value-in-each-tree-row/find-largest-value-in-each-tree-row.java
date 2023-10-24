e57f4209cee0b7768bda74c3a3bdf792989cb4b5
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-10-24 22:31:10 +0530

    Time: 7 ms (5.38%) | Memory: 43.9 MB (83.55%) - LeetSync

---
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -0,0 +1,82 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public List<Integer> largestValues(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        q.offer(root);
+        int level = 1;
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> currentlevel = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode curr = q.poll();
+                currentlevel.add(curr.val);
+                if (curr.left != null) {
+                    q.offer(curr.left); // Adding the left part of Node
+                }
+                if (curr.right != null) { // Adding the Right part of Node
+                    q.offer(curr.right);
+                }
+            }
+
+            Collections.reverse(currentlevel);
+            ans.add(currentlevel.stream().max(Comparator.comparingInt(Integer::intValue)).orElse(0));
+
+            //  ans.add(currentlevel);
+            level++;
+        }
+        return ans;
+    }
+}
+
+// class Solution {
+
+//     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+//         Queue<TreeNode> q = new LinkedList<>();
+//         ArrayList<List<Integer>> ans = new ArrayList<>();
+//         if (root == null) {
+//             return ans;
+//         }
+//         q.offer(root);
+//         int level = 1;
+//         while (!q.isEmpty()) {
+//             int levelSize = q.size();
+//             List<Integer> currentlevel = new ArrayList<>();
+//             for (int i = 0; i < levelSize; i++) {
+//                 TreeNode curr = q.poll();
+//                 currentlevel.add(curr.val);
+//                 if (curr.left != null) {
+//                     q.offer(curr.left); // Adding the left part of Node
+//                 }
+//                 if (curr.right != null) { // Adding the Right part of Node
+//                     q.offer(curr.right);
+//                 }
+//             }
+
+//             Collections.reverse(currentlevel);
+//             ans.add(currentlevel.stream().max(Comparator.comparingInt(Integer::intValue)));
+
+//             //  ans.add(currentlevel);
+//             level++;
+//         }
+//         return ans;
+//     }
+// }
