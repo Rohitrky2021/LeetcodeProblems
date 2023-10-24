b30d1533cf894b4d7848111ad4013cf80607c9f6
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-10-24 22:56:39 +0530

    Time: 2 ms (82.84%) | Memory: 44 MB (61.01%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -13,8 +13,41 @@
  *     }
  * }
  */
-class Solution {
+// class Solution {
 
+//     public List<Integer> largestValues(TreeNode root) {
+//         Queue<TreeNode> q = new LinkedList<>();
+//         ArrayList<Integer> ans = new ArrayList<>();
+//         if (root == null) {
+//             return ans;
+//         }
+//         q.offer(root);
+     
+//         while (!q.isEmpty()) {
+//             int max=Integer.MIN_VALUE;
+//             int levelSize = q.size();
+ 
+//             for (int i = 0; i < levelSize; i++) {
+//                 TreeNode curr = q.poll();
+  
+//                 max=Math.max(max,curr.val);
+//                 if (curr.left != null) {
+
+//                     q.offer(curr.left); // Adding the left part of Node
+//                 }
+//                 if (curr.right != null) { // Adding the Right part of Node
+//                     q.offer(curr.right);
+//                 }
+//             }
+ 
+//             ans.add(max);
+ 
+//         }
+//         return ans;
+//     }
+// }
+
+class Solution {
     public List<Integer> largestValues(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
         ArrayList<Integer> ans = new ArrayList<>();
@@ -24,24 +57,21 @@ class Solution {
         q.offer(root);
      
         while (!q.isEmpty()) {
-            int max=Integer.MIN_VALUE;
             int levelSize = q.size();
+            int max = Integer.MIN_VALUE; // Initialize max for the current level
  
             for (int i = 0; i < levelSize; i++) {
                 TreeNode curr = q.poll();
-  
-                max=Math.max(max,curr.val);
+                max = Math.max(max, curr.val);
                 if (curr.left != null) {
-
                     q.offer(curr.left); // Adding the left part of Node
                 }
-                if (curr.right != null) { // Adding the Right part of Node
-                    q.offer(curr.right);
+                if (curr.right != null) {
+                    q.offer(curr.right); // Adding the Right part of Node
                 }
             }
  
-            ans.add(max);
- 
+            ans.add(max); // Add the maximum value for the current level to the result list
         }
         return ans;
     }
