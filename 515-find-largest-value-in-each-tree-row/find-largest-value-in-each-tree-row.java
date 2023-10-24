b98e2802802ae6daf927a5529e11f8ba1f8df34d
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-10-24 22:59:19 +0530

    Time: 2 ms (82.84%) | Memory: 43.7 MB (95.01%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -13,41 +13,75 @@
  *     }
  * }
  */
-// class Solution {
 
-//     public List<Integer> largestValues(TreeNode root) {
-//         Queue<TreeNode> q = new LinkedList<>();
-//         ArrayList<Integer> ans = new ArrayList<>();
-//         if (root == null) {
-//             return ans;
-//         }
-//         q.offer(root);
+ class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
+        
+        List<Integer> ans = new ArrayList<Integer>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            int currentLength = queue.size();
+            int currMax = Integer.MIN_VALUE;
+            
+            for (int i = 0; i < currentLength; i++) {
+                TreeNode node = queue.remove();
+                currMax = Math.max(currMax, node.val);
+                
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            
+            ans.add(currMax);
+        }
+        
+        return ans;
+    }
+}
+class Solution2 {
+
+    public List<Integer> largestValues(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        q.offer(root);
      
-//         while (!q.isEmpty()) {
-//             int max=Integer.MIN_VALUE;
-//             int levelSize = q.size();
+        while (!q.isEmpty()) {
+            int max=Integer.MIN_VALUE;
+            int levelSize = q.size();
  
-//             for (int i = 0; i < levelSize; i++) {
-//                 TreeNode curr = q.poll();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode curr = q.poll();
   
-//                 max=Math.max(max,curr.val);
-//                 if (curr.left != null) {
+                max=Math.max(max,curr.val);
+                if (curr.left != null) {
 
-//                     q.offer(curr.left); // Adding the left part of Node
-//                 }
-//                 if (curr.right != null) { // Adding the Right part of Node
-//                     q.offer(curr.right);
-//                 }
-//             }
+                    q.offer(curr.left); // Adding the left part of Node
+                }
+                if (curr.right != null) { // Adding the Right part of Node
+                    q.offer(curr.right);
+                }
+            }
  
-//             ans.add(max);
+            ans.add(max);
  
-//         }
-//         return ans;
-//     }
-// }
+        }
+        return ans;
+    }
+}
 
-class Solution {
+class Solution3 {
     public List<Integer> largestValues(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
         ArrayList<Integer> ans = new ArrayList<>();
