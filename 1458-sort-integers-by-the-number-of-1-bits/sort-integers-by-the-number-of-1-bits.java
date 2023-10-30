83b423894e710f5787d6ed8c26c087ce6d91ccb6
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-10-30 15:24:30 +0530

    Time: 9 ms (47.46%) | Memory: 43.5 MB (89.02%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -2,38 +2,32 @@ import java.util.*;
 
 class Solution {
 
- public int[] sortByBits(int[] arr) {
+    public int[] sortByBits(int[] arr) {
         // Convert the primitive int array to an Integer array
-        Integer[] integerArr = new Integer[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            integerArr[i] = arr[i];
-        }
+        Integer[] integerArr = Arrays.stream(arr).boxed().toArray(Integer[]::new);
 
-        // Define a custom comparator to compare integers by the number of set bits
-        Comparator<Integer> bitComparator = new Comparator<Integer>() {
-            @Override
-            public int compare(Integer a, Integer b) {
-                int bitCountA = Integer.bitCount(a); // Count set bits in 'a'
-                int bitCountB = Integer.bitCount(b); // Count set bits in 'b'
-                
-                // If the number of set bits is the same, compare the integers themselves
-                if (bitCountA == bitCountB) {
-                    return a - b;
-                }
-                
-                // Otherwise, sort by the number of set bits
-                return bitCountA - bitCountB;
+        // Define a custom comparator using a lambda function
+        Comparator<Integer> bitComparator = (a, b) -> {
+            int bitCountA = Integer.bitCount(a);
+            int bitCountB = Integer.bitCount(b);
+
+            // If the number of set bits is the same, compare the integers themselves
+            if (bitCountA == bitCountB) {
+                return a - b;
             }
+
+            // Otherwise, sort by the number of set bits
+            return bitCountA - bitCountB;
         };
-        
+
         // Sort the Integer array using the custom comparator
         Arrays.sort(integerArr, bitComparator);
-        
+
         // Convert the sorted Integer array back to a primitive int array
         for (int i = 0; i < arr.length; i++) {
             arr[i] = integerArr[i];
         }
-        
+
         return arr;
     }
 }
