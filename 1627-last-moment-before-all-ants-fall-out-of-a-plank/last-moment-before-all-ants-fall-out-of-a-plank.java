4646ce88b53c63929a5493bda7287f936a88db74
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-11-05 18:32:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (42.86%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.java
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 
     public int getLastMoment(int n, int[] left, int[] right) {
         Arrays.sort(left);
@@ -10,5 +10,21 @@ class Solution {
         if (right.length == 0) return left[left.length - 1];
 
         return -1;
+    }
+}
+
+
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int ans = 0;
+        for (int num : left) {
+            ans = Math.max(ans, num);
+        }
+        
+        for (int num : right) {
+            ans = Math.max(ans, n - num);
+        }
+        
+        return ans;
     }
 }
