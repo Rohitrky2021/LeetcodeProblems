09b55a126524d2330ac8246a7377b5068ceefb04
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-11-08 13:18:13 +0530

    Time: 1 ms (99.88%) | Memory: 40.8 MB (95.11%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int longestValidParentheses(String s) {
         Stack<Integer> stack = new Stack<>();
         stack.push(-1);  // Initialize the stack with -1 as a starting point.
@@ -12,7 +12,7 @@ class Solution {
             } else {
                 stack.pop();
                 if (stack.isEmpty()) {
-                    stack.push(i);  // Starting point for the next valid substring.
+                    stack.push(i);  // Starting pointa for the next valid substring.
                 } else {
                     maxLen = Math.max(maxLen, i - stack.peek());
                 }
@@ -22,3 +22,42 @@ class Solution {
         return maxLen;
     }
 }
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        int left = 0, right = 0, maxLen = 0;
+
+        // Forward pass - check from left to right
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxLen = Math.max(maxLen, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+
+        left = right = 0;
+
+        // Backward pass - check from right to left
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxLen = Math.max(maxLen, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+
+        return maxLen;
+    }
+}
+
