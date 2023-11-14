805dee724e7889c8d287ae9a0e2a62e12e5665e0
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-11-14 10:39:23 +0530

    Time: 0 ms (100.00%) | Memory: 39.1 MB (40.27%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        boolean dp[]=new boolean[n];
+        // if(n==2){
+        //     return true;
+        // }
+
+        if(n%2==0)return true;
+
+        // for(int i=0;i<n;i++){
+
+        // }
+
+        return false;
+
+    }
+}
