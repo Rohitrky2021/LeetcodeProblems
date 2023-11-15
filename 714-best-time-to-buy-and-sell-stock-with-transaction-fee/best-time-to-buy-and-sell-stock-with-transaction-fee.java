c47ac76900fc15c90914d96564e63cbf567e61cf
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-11-16 02:32:02 +0530

    Time: 41 ms (5.02%) | Memory: 68.8 MB (5.14%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,35 +1,32 @@
 class Solution {
-
     public int maxProfit(int[] prices, int fee) {
-          Map<String, Integer> cache = new HashMap<>();
+        int n = prices.length;
+        Integer[][] cache = new Integer[n][2];
         return dfsWithTransactionFee(0, true, 0, prices, fee, cache);
     }
 
-    private int dfsWithTransactionFee(int currIdx, boolean canBuy, int numTxns, int[] prices, int fee, Map<String, Integer> cache) {
+    private int dfsWithTransactionFee(int currIdx, boolean canBuy, int numTxns, int[] prices, int fee, Integer[][] cache) {
         if (currIdx >= prices.length) {
             return 0;
         }
 
-        String key = currIdx + "-" + canBuy ;
-        if (cache.containsKey(key)) {
-            return cache.get(key);
+        if (cache[currIdx][canBuy ? 1 : 0] != null) {
+            return cache[currIdx][canBuy ? 1 : 0];
         }
 
         // do nothing
         int nothing = dfsWithTransactionFee(currIdx + 1, canBuy, numTxns, prices, fee, cache);
 
-        // buy
-        int actionBuy;
+        // buy or sell
+        int actionBuySell;
         if (canBuy) {
-            actionBuy = dfsWithTransactionFee(currIdx + 1, false, numTxns, prices, fee, cache) - prices[currIdx] - fee;
+            actionBuySell = dfsWithTransactionFee(currIdx + 1, false, numTxns, prices, fee, cache) - prices[currIdx] - fee;
         } else {
- 
-        // sell
-        actionBuy = dfsWithTransactionFee(currIdx + 1, true, numTxns + 1, prices, fee, cache) + prices[currIdx];
+            actionBuySell = dfsWithTransactionFee(currIdx + 1, true, numTxns + 1, prices, fee, cache) + prices[currIdx];
         }
-        int best =Math.max(nothing, actionBuy);
-        cache.put(key, best);
+
+        int best = Math.max(nothing, actionBuySell);
+        cache[currIdx][canBuy ? 1 : 0] = best;
         return best;
     }
-
 }
