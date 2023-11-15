72e8efd9359c00a169f025a3fd7204d0613db634
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-11-16 02:14:17 +0530

    Time: 877 ms (5.01%) | Memory: 167.5 MB (5.02%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+     Map<String, Integer> cache = new HashMap<>();
+        return dfs(0, true, 0, 2, prices, cache);
+    }
+
+    private int dfs(int currIdx, boolean canBuy, int numTxns, int k, int[] prices, Map<String, Integer> cache) {
+        if (numTxns >= k || currIdx == prices.length) {
+            return 0;
+        }
+
+        String key = currIdx + "-" + canBuy + "-" + numTxns;
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
+
+        // do nothing
+        int nothing = dfs(currIdx + 1, canBuy, numTxns, k, prices, cache);
+
+        // buy
+        int action;
+        if (canBuy) {
+            action = dfs(currIdx + 1, false, numTxns, k, prices, cache) - prices[currIdx];
+        }
+        // sell
+        else {
+            action = dfs(currIdx + 1, true, numTxns + 1, k, prices, cache) + prices[currIdx];
+        }
+
+        int best = Math.max(nothing, action);
+        cache.put(key, best);
+        return best;
+    }
+}
