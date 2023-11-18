cc565803192ad837a91107a780522eceb7461873
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-11-18 20:46:48 +0530

    Time: 3 ms (100.00%) | Memory: 55.6 MB (40.64%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solutions {
     public int[] sortArray(int[] nums) {
 
         int i=0;
@@ -8,4 +8,35 @@ class Solution {
         while(!pq.isEmpty()) ans[i++] = pq.poll();
         return ans;
     }
+}
+
+class Solution {
+    public int[] sortArray(int[] a) {
+        int max = a[0];
+        int min = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if(max<a[i]){
+                max = a[i];
+            }
+        }
+        for (int i = 1; i < a.length; i++) {
+            if(min>a[i]){
+                min = a[i];
+            }
+        }
+        int[] count = new int[max-min+1];
+        for(int v:a){
+            count[v-min]++;
+        }
+        int k=0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i]>0){
+                a[k] = i+min;
+                k++;
+                count[i]--;
+            }
+        }
+        return a;
+    }
+
 }
