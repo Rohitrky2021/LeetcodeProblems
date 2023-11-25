eb6d2a4e60386a6d464d61d47ff0b51f413f76fd
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-11-26 00:51:39 +0530

    Time: 1 ms (83.95%) | Memory: 43.6 MB (5.01%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int[] fre = new int[1000];
+        int occu = 0;
+
+        // Count occurrences of each element in nums
+        for (int num : nums) {
+            fre[num]++;
+        }
+
+        // Calculate the total occurrences of elements smaller than target
+        for (int i = 0; i < target; i++) {
+            occu += fre[i];
+        }
+
+        // Add indices of the target element to the result in sorted order
+        for (int i = 0; i < fre[target]; i++) {
+            ans.add(occu + i);
+        }
+
+        return ans;
+    }
+}
