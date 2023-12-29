e93fb333001315aae64041c3585a2258ed0087db
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2023-12-29 19:14:37 +0530

    Time: 11 ms (45.85%) | Memory: 42.3 MB (46.20%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,5 +1,73 @@
 public class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
+        }
+
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] == s2map[i])
+                count++;
+        }
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
+            if (count == 26)
+                return true;
+            s2map[r]++;
+            if (s2map[r] == s1map[r]) {
+                count++;
+            } else if (s2map[r] == s1map[r] + 1) {
+                count--;
+            }
+            s2map[l]--;
+            if (s2map[l] == s1map[l]) {
+                count++;
+            } else if (s2map[l] == s1map[l] - 1) {
+                count--;
+            }
+        }
+        return count == 26;
+    }
+}
+
+
+  class Solution4 {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        HashMap<Character, Integer> s1map = new HashMap<>();
+
+        for (int i = 0; i < s1.length(); i++)
+            s1map.put(s1.charAt(i), s1map.getOrDefault(s1.charAt(i), 0) + 1);
+
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            HashMap<Character, Integer> s2map = new HashMap<>();
+            for (int j = 0; j < s1.length(); j++) {
+                s2map.put(s2.charAt(i + j), s2map.getOrDefault(s2.charAt(i + j), 0) + 1);
+            }
+            if (matches(s1map, s2map))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean matches(HashMap<Character, Integer> s1map, HashMap<Character, Integer> s2map) {
+        for (char key : s1map.keySet()) {
+            if (s1map.get(key) - s2map.getOrDefault(key, -1) != 0)
+                return false;
+        }
+        return true;
+    }
+}
+
+  class Solution3 {
+    public boolean checkInclusion(String s1, String s2) {
         s1 = sort(s1);
         for (int i = 0; i <= s2.length() - s1.length(); i++) {
             if (s1.equals(sort(s2.substring(i, i + s1.length()))))
@@ -95,4 +163,31 @@ class Solution1 {
         return true;
     }
 
+}
+
+   class Solution0 {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        for (int i = 0; i < s1.length(); i++)
+            s1map[s1.charAt(i) - 'a']++;
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            int[] s2map = new int[26];
+            for (int j = 0; j < s1.length(); j++) {
+                s2map[s2.charAt(i + j) - 'a']++;
+            }
+            if (matches(s1map, s2map))
+                return true;
+        }
+        return false;
+    }
+    
+    public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
 }
