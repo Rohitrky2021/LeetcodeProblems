e3172e8f9cc40d2b3d47aa56c4ae14316c13870c
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-01-05 17:38:17 +0530

    Time: 333 ms (10.26%) | Memory: 75.6 MB (13.75%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Solution {
+class Solution2 {
  
     public static int lengthOfLIS(int[] nums) {
         int n = nums.length;
@@ -29,3 +29,63 @@ class Solution {
         return mx;
     }
 }
+
+ 
+public class Solution {
+
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] distinctSortedNums = removeDuplicatesAndSort(nums);
+        int[][] memo = new int[n + 1][distinctSortedNums.length + 1];
+
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+
+        return lcs(nums, distinctSortedNums, n, distinctSortedNums.length, memo);
+    }
+
+    private int lcs(int[] arr1, int[] arr2, int n, int m, int[][] memo) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+
+        if (memo[n][m] != -1) {
+            return memo[n][m];
+        }
+
+        if (arr1[n - 1] == arr2[m - 1]) {
+            return memo[n][m] = 1 + lcs(arr1, arr2, n - 1, m - 1, memo);
+        } else {
+            int ans1 = lcs(arr1, arr2, n - 1, m, memo);
+            int ans2 = lcs(arr1, arr2, n, m - 1, memo);
+            return memo[n][m] = Math.max(ans1, ans2);
+        }
+    }
+
+    private int[] removeDuplicatesAndSort(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int[] distinctSortedNums = new int[set.size()];
+        int index = 0;
+        for (int num : set) {
+            distinctSortedNums[index++] = num;
+        }
+
+        Arrays.sort(distinctSortedNums);
+        return distinctSortedNums;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // arr = array of elements
+        int[] arr = {50, 3, 10, 7, 40, 80};
+
+        System.out.println("Size of the longest increasing subsequence: " + solution.lengthOfLIS(arr));
+    }
+}
+
