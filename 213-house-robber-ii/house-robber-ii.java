666ba237c8b965dc2b701baccc7fbf37bbe568d5
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-01-08 14:45:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (25.27%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,27 +1,34 @@
-class Solution2 {
  
+class Solution {
 
     public int rob(int[] nums) {
-        if (nums.length < 1) return 0;
-        if (nums.length < 2) return nums[0];
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
 
-        int[] Rub = new int[nums.length];
-        Rub[0] = nums[0];
-        Rub[1] = Math.max(nums[1], Rub[0]);
+        return Math.max(
+            robHelper(nums, 0, nums.length - 2),
+            robHelper(nums, 1, nums.length - 1)
+        );
+    }
 
-        for (int i = 2; i < nums.length; i++) {
-            Rub[i] = Math.max(Rub[i - 2] + nums[i], Rub[i - 1]);
+    public int robHelper(int[] nums, int start, int end) {
+        int rob1 = 0;
+        int rob2 = 0;
+        for (int i = start; i <= end; i++) {
+            int temp = Math.max(rob1 + nums[i], rob2);
+            rob1 = rob2;
+            rob2 = temp;
         }
-
-        return Rub[nums.length - 1];
+        return rob2;
     }
 }
 
 
 
+
  
 
-class Solution {
+class Solution3 {
     public int rob(int[] nums) {
         int n = nums.length;
 
