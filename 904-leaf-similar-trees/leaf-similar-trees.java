60be4fd5a627d53decadfeb61e6cd44b0b388bfe
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-01-09 13:59:04 +0530

    Time: 16 ms (5.79%) | Memory: 41.6 MB (13.01%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.java
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -1,7 +1,65 @@
+class Solution1 {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList();
+        List<Integer> leaves2 = new ArrayList();
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+        return leaves1.equals(leaves2);
+    }
+
+    public void dfs(TreeNode node, List<Integer> leafValues) {
+        if (node != null) {
+            if (node.left == null && node.right == null)
+                leafValues.add(node.val);
+            dfs(node.left, leafValues);
+            dfs(node.right, leafValues);
+        }
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution2 {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList();
+        List<Integer> leaves2 = new ArrayList();
+         collectLeaves(root1, leaves1);
+        collectLeaves(root2, leaves2);
+        return leaves1.equals(leaves2);
+    }
+
+   private void collectLeaves(TreeNode node, List<Integer> leafValues) {
+        if (node == null) {
+            return;
+        }
+        
+        if (node.left == null && node.right == null) {
+            leafValues.add(node.val);
+        } else {
+            collectLeaves(node.left, leafValues);
+            collectLeaves(node.right, leafValues);
+        }
+    }
+}
+
+
 class Solution {
 
-    ArrayList<TreeNode> r1 = new ArrayList<>();
-    ArrayList<TreeNode> r2 = new ArrayList<>();
+    ArrayList<Integer> r1 = new ArrayList<>();
+    ArrayList<Integer> r2 = new ArrayList<>();
 
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
 
@@ -9,24 +67,26 @@ class Solution {
         leaf2(root2);
 
         System.out.print("r1: ");
-        for (TreeNode node : r1) {
-            System.out.print(node.val + " ");
+        for (int value : r1) {
+            System.out.print(value + " ");
         }
         System.out.println();
 
-         for (TreeNode node : r2) {
-            System.out.print(node.val + " ");
+        System.out.print("r2: ");
+        for (int value : r2) {
+            System.out.print(value + " ");
         }
 
-        return valuesEqual(r1, r2);
+        return r1.equals(r2);
     }
-        private boolean valuesEqual(ArrayList<TreeNode> list1, ArrayList<TreeNode> list2) {
+
+    private boolean valuesEqual(ArrayList<Integer> list1, ArrayList<Integer> list2) {
         if (list1.size() != list2.size()) {
             return false;
         }
 
         for (int i = 0; i < list1.size(); i++) {
-            if (list1.get(i).val != list2.get(i).val) {
+            if (!list1.get(i).equals(list2.get(i))) {
                 return false;
             }
         }
@@ -36,7 +96,7 @@ class Solution {
 
     public void leaf2(TreeNode ro1) {
         if (ro1 != null) {
-            if (ro1.left == null && ro1.right == null) r2.add(ro1);
+            if (ro1.left == null && ro1.right == null) r2.add(ro1.val);
             leaf2(ro1.left);
             leaf2(ro1.right);
         }
@@ -44,19 +104,10 @@ class Solution {
 
     public void leaf(TreeNode ro1) {
         if (ro1 != null) {
-            if (ro1.left == null && ro1.right == null) r1.add(ro1);
+            if (ro1.left == null && ro1.right == null) r1.add(ro1.val);
             leaf(ro1.left);
             leaf(ro1.right);
         }
     }
-
-    public static void main(String[] args) {
-        TreeNode root1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
-        TreeNode root2 = new TreeNode(1, new TreeNode(3), new TreeNode(2));
-
-        Solution solution = new Solution();
-        boolean result = solution.leafSimilar(root1, root2);
-
-        System.out.println("Are the leaf sequences similar? " + result);
-    }
+ 
 }
