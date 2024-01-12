ebfeaa487829e6cfb15abd054b60ecb0c83b628a
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-01-12 09:46:04 +0530

    Time: 5 ms (51.60%) | Memory: 41.9 MB (24.13%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution2 {
     public boolean halvesAreAlike(String s) {
         // String s = sc.nextLine();
         boolean a = false;
@@ -27,5 +27,33 @@ class Solution {
 
         return count == 0;
 
+    }
+}
+
+
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        char[] charArray = s.toLowerCase().toCharArray();
+        int count = 0;
+
+        // Count vowels in the second half of the char array
+        for (int i = charArray.length / 2; i < charArray.length; i++) {
+            char currentChar = charArray[i];
+
+            if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u') {
+                count++;
+            }
+        }
+
+        // Subtract vowels in the first half of the char array
+        for (int i = 0; i < charArray.length / 2; i++) {
+            char currentChar = charArray[i];
+
+            if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u') {
+                count--;
+            }
+        }
+
+        return count == 0;
     }
 }
