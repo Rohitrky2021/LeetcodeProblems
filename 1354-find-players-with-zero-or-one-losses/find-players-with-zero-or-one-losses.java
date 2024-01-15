97714b4421949428643f1e4fcf2df61592de0885
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-01-15 18:59:10 +0530

    Time: 56 ms (90.98%) | Memory: 92 MB (61.27%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -11,6 +11,48 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> winnersFirstColumn = new ArrayList<>();
         List<Integer> winnersSecondColumn = new ArrayList<>();
+
+        int[] frequencyFirstColumn = new int[100001];
+        int[] frequencySecondColumn = new int[100001];
+
+        // Count the frequency of each element in the second column
+        for (int[] match : matches) {
+            frequencySecondColumn[match[1]]++;
+        }
+
+        // Find winners in the first column
+        for (int[] match : matches) {
+            frequencyFirstColumn[match[0]]++;
+            if (frequencySecondColumn[match[0]] == 0) {
+                winnersFirstColumn.add(match[0]);
+            }
+        }
+
+        // Sort and make winnersFirstColumn unique
+        Set<Integer> uniqueSortedWinnersFirstColumn = new TreeSet<>(winnersFirstColumn);
+
+        // Find winners in the second column
+        for (int i = 1; i <= 100000; i++) {
+            if (frequencySecondColumn[i] == 1) {
+                winnersSecondColumn.add(i);
+            }
+        }
+
+        ans.add(new ArrayList<>(uniqueSortedWinnersFirstColumn));
+        ans.add(winnersSecondColumn);
+
+        return ans;
+    }
+}
+
+ 
+ 
+
+class Solution2 {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> winnersFirstColumn = new ArrayList<>();
+        List<Integer> winnersSecondColumn = new ArrayList<>();
         TreeMap<Integer, Integer> frequencyMap = new TreeMap<>();
 
         // Count the frequency of each element in the second column
