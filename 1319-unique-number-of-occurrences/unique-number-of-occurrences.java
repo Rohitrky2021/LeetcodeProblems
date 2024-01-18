952230156c18ac3e852af54aaa03c4fe3082efdc
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-01-18 10:25:42 +0530

    Time: 2 ms (98.10%) | Memory: 42.2 MB (5.65%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public boolean uniqueOccurrences(int[] arr) {
         int f[] = new int[2020];
 
@@ -15,5 +15,32 @@ class Solution {
         }
         return true;
 
+    }
+}
+
+// import java.util.HashSet;
+// import java.util.Set;
+
+class Solution {
+    private static final int OFFSET = 1000;
+
+    public boolean uniqueOccurrences(int[] arr) {
+        int[] f = new int[2020];
+
+        for (int num : arr) {
+            f[num + OFFSET]++;
+        }
+
+        Set<Integer> occurrencesSet = new HashSet<>();
+
+        for (int count : f) {
+            if (count != 0) {
+                if (!occurrencesSet.add(count)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
