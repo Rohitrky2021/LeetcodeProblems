4fe2eac69f8b27e18c8487390b4c9cc04820b5c4
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-01-30 00:52:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (29.67%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,10 +1,10 @@
 class MyQueue {
 
-    Stack<Integer> s1;
+    Stack<Integer> s1= new Stack<>();
     Stack<Integer> s2;
 
     public MyQueue() {
-        s1 = new Stack<>();
+        // s1 = new Stack<>();
         s2 = new Stack<>();
     }
 
