8a6051f444933d3d63a573c0a2b122e4a7ed009f
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-01 01:10:57 +0530

    Time: 77 ms (35.52%) | Memory: 59.9 MB (56.43%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,31 +1,20 @@
 class Solution {
     public int[] dailyTemperatures(int[] arr) {
-        Stack<Integer> ss = new Stack<>();
-        int n=arr.length;
-        int anss[] = new int[n];
-        // int arr[] = { 6, 8, 0, 1, 3 };
-        ArrayList<Integer> ans = new ArrayList<>();
+    Stack<Integer> ss = new Stack<>();
+        int n = arr.length;
+        int[] anss = new int[n];
+
         for (int i = n - 1; i >= 0; i--) {
             while (!ss.isEmpty() && arr[ss.peek()] <= arr[i]) {
                 ss.pop();
             }
             if (ss.isEmpty()) {
-                // ss.push(-1);
-                // ans.add(0);
-                anss[i]=0;
+                anss[i] = 0;
             } else {
-                // ans.add( ss.peek()-i);
-                 anss[i]=ss.peek()-i;
+                anss[i] = ss.peek() - i;
             }
-            ss.push(i);// index se check krenge value
+            ss.push(i);
         }
-
-        
-        // for (int i =0; i<n; i++) {
-        //     // System.out.print(ans.get(i) + " ");
-        //     anss[n-i-1]=ans.get(i);
-        // }
-
 
         return anss;
 
