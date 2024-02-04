6c6d046d1395d05d8d5669fcc20be90a5135ea18
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-04 22:00:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (56.03%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numTrees(int n) {
+        return catalanTABULAT(n);
+    }
+
+        public static int catalanTABULAT(int n){
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+
+        for (int i = 2; i < n+1; i++) {
+            for (int j = 0; j < i; j++) {
+                int left=dp[j];
+                int right=dp[i-j-1];//Ci = Cj * Ci-j-1;
+                dp[i]+=left*right;
+            }
+        }
+        return dp[n];
+    }
+}
