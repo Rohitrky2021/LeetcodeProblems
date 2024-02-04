fbfc5afc471af887cacd9d4dd0224f642d1d7273
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-05 03:03:36 +0530

    Time: 12 ms (38.60%) | Memory: 45.6 MB (51.65%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,45 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
-
-    public int[] findErrorNums(int[] arr) {
-        // Integer[] real = ans.toArray(new Integer[0]);
-        List<Integer> ans = new ArrayList<>();
-        CycleSort(arr, arr.length);
-        int i = 0;
-        for (i = 0; i < arr.length; i++) {
-            if (arr[i] != i + 1) {
-                // return i;
-                ans.add(arr[i]);
-                ans.add(i+1);
+    public int[] findErrorNums(int[] nums) {
+        Arrays.sort(nums);
+        int[] result = new int[2];
+        int n = nums.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                result[0] = nums[i]; // Duplicate number
+                break;
             }
         }
-        int[] real = new int[ans.size()];
-        for (i = 0; i < ans.size(); i++) {
-            real[i] = ans.get(i);
+        
+        long sum = (long) n * (n + 1) / 2;
+        for (int num : nums) {
+            sum -= num;
         }
-        if (ans.isEmpty()) {
-            // ans.add(i);
-        }
-        return real;
-    }
-
-    static void CycleSort(int[] arr, int n) {
-        int i = 0;
-        while (i < n) {
-            int correctpos = arr[i] - 1;
-            if (arr[i] <= n && arr[i] != arr[correctpos]) {
-                swap(arr, i, correctpos);
-            } else {
-                i++;
-            }
-        }
-        System.out.println("After sort : ");
-        System.out.print(Arrays.toString(arr));
-    }
-
-    static void swap(int[] arr, int i, int correctpos) {
-        // swap elements with their correct indexes
-        int temp = arr[i];
-        arr[i] = arr[correctpos];
-        arr[correctpos] = temp;
+        
+        result[1] = (int) (result[0] + sum); // Missing number
+        
+        return result;
     }
 }
