8dc6e05fea0e51035bfbcd3c4ff0619516c622a7
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-05 16:58:25 +0530

    Time: 7 ms (70.75%) | Memory: 67.3 MB (35.10%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+
+class Solution {
+    public int makeConnected(int n, int[][] cns) {
+        int m = cns.length;
+        HashSet<Integer> s=new HashSet<>();
+
+        if(m<n-1) return -1;
+        UnionFind dsu = new UnionFind(n);
+
+        for(int []x:cns){
+            dsu.union(x[0],x[1]);
+        }
+
+        for(int i=0;i<n;i++){
+            s.add(dsu.find(i));
+        }
+
+        return s.size()-1;
+    }
+
+    class UnionFind {
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(int size) {
+            parent = new int[size];
+            for (int i = 0; i < size; i++)
+                parent[i] = i;
+            rank = new int[size];
+        }
+
+        public int find(int x) {
+            if (parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            int xset = find(x), yset = find(y);
+            if (xset == yset) {
+                return;
+            } else if (rank[xset] < rank[yset]) {
+                parent[xset] = yset;
+            } else if (rank[xset] > rank[yset]) {
+                parent[yset] = xset;
+            } else {
+                parent[yset] = xset;
+                rank[xset]++;
+            }
+        }
+    }
+}
