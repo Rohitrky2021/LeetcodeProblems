db1c0f8b7e67beae7b0a29d94ba93faea5d63c58
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-05 23:36:41 +0530

    Time: 6 ms (92.14%) | Memory: 42.2 MB (35.51%) - LeetSync

---
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -0,0 +1,55 @@
+ 
+
+class Solution {
+    private int maxSum(int[] arr, int k, int[] dp, int start) {
+        int N = arr.length;
+
+        if (start >= N) {
+            return 0;
+        }
+
+        // Return the already calculated answer.
+        if (dp[start] != -1) {
+            return dp[start];
+        }
+
+        int currMax = 0, ans = 0;
+        int end = Math.min(N, start + k);
+        for (int i = start; i < end; i++) {
+            currMax = Math.max(currMax, arr[i]);
+            // Store the maximum of all options for the current subarray.
+            ans = Math.max(ans, currMax * (i - start + 1) + maxSum(arr, k, dp, i + 1));
+        }
+
+        // Store the answer to be reused.
+        return dp[start] = ans;
+    }
+
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int[] dp = new int[arr.length];
+        Arrays.fill(dp, -1);
+
+        return maxSum(arr, k, dp, 0);
+    }
+}
+
+
+// M2 -- USing Botttom up 
+class Solution2 {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int maximum = 0;
+            int maxSum = 0;
+            for (int j = 1; j <= k && i - j >= 0; j++) {
+                maximum = Math.max(maximum, arr[i - j]);
+                maxSum = Math.max(maxSum, dp[i - j] + maximum * j);
+            }
+            dp[i] = maxSum;
+        }
+
+        return dp[n];
+    }
+}
