88205732cc4a34bba296201be55654141029220c
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-13 19:30:13 +0530

    Time: 24 ms (46.23%) | Memory: 44.7 MB (65.70%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,7 +1,37 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return tabu(  s,  p, s.length(), p.length());
+
+        // M1 
+        // return tabu(  s,  p, s.length(), p.length());
+
+    //  M2
+        boolean dp[][] = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][0] = false;
+        }
+        for (int j = 1; j < dp[0].length; j++) {
+            if (p.charAt(j-1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }else{
+                dp[0][j] = false;
+            }
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        // printDp(dp);
+        return dp[s.length()][p.length()];
     }
+ 
 
         public static boolean tabu(String s, String p, int n, int m){
         boolean[][] dp = new boolean[n+1][m+1];
