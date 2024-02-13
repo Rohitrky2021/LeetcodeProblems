f3410e1e5d3a6f870bb68d673442397ff90e0e9a
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-13 22:59:10 +0530

    Time: 368 ms (5.18%) | Memory: 88 MB (5.05%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -33,11 +33,12 @@ class Solution2 {
     }
 
 }
+ 
 
 class Solution {
 
     public int maxProfit(int[] prices, int fee) {
-          Map<String, Integer> cache = new HashMap<>();
+        Map<String, Integer> cache = new HashMap<>();
         return dfsWithTransactionFee(0, true, 0, prices, fee, cache);
     }
 
@@ -46,40 +47,30 @@ class Solution {
             return 0;
         }
 
-        String key = currIdx + "-" + canBuy ;
+        String key = currIdx + "-"+ canBuy;
         if (cache.containsKey(key)) {
             return cache.get(key);
         }
 
-        // do nothing
-       
+        int profit = 0;
 
-        // buy
-        int actionBuy;
-        int best=0;
-        //  int nothing 
         if (canBuy) {
-            actionBuy = dfsWithTransactionFee(currIdx + 1, false, numTxns, prices, fee, cache) - prices[currIdx] - fee;
-             int nothing = dfsWithTransactionFee(currIdx + 1, canBuy, numTxns, prices, fee, cache);
-             best =Math.max(nothing, actionBuy);
-             cache.put(key, best);
+            // Either we skip this buy, or we buy
+            int skipBuy = dfsWithTransactionFee(currIdx + 1, true, numTxns, prices, fee, cache);
+            int buy = dfsWithTransactionFee(currIdx + 1, false, numTxns, prices, fee, cache) - prices[currIdx] - fee;
+            profit = Math.max(skipBuy, buy);
         } else {
- 
-        // sell
-        actionBuy = dfsWithTransactionFee(currIdx + 1, true, numTxns + 1, prices, fee, cache) + prices[currIdx];
-         int nothing = dfsWithTransactionFee(currIdx + 1, canBuy, numTxns, prices, fee, cache);
-            best =Math.max(nothing, actionBuy);
-            cache.put(key, best);
-
-
-
+            // Either we skip this sell, or we sell
+            int skipSell = dfsWithTransactionFee(currIdx + 1, false, numTxns + 1, prices, fee, cache);
+            int sell = dfsWithTransactionFee(currIdx + 1, true, numTxns, prices, fee, cache) + prices[currIdx];
+            profit = Math.max(skipSell, sell);
         }
-          
-        
-        return best;
-    }
 
+        cache.put(key, profit);
+        return profit;
+    }
 }
+
 
 
 
