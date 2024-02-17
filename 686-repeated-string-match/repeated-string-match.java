2c4d8c651eb4f41e57e00630c1552d69f1dfa936
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-17 13:24:51 +0530

    Time: 265 ms (59.90%) | Memory: 41.8 MB (96.58%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.java
@@ -0,0 +1,58 @@
+// Rabin karps Algo -- >  Hashing of String for Pattern Matching 
+ import java.math.BigInteger;
+
+class Solution {
+    public int repeatedStringMatch(String A, String B) {
+        int q = 1;
+        StringBuilder S = new StringBuilder(A);
+        for (; S.length() < B.length(); q++) S.append(A);
+        if (S.indexOf(B) >= 0) return q;
+        if (S.append(A).indexOf(B) >= 0) return q+1;
+        return -1;
+    }
+}
+ 
+
+class Solution2 {
+    public boolean check(int index, String A, String B) {
+        for (int i = 0; i < B.length(); i++) {
+            if (A.charAt((i + index) % A.length()) != B.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int repeatedStringMatch(String A, String B) {
+        int q = (B.length() - 1) / A.length() + 1;
+        int p = 113, MOD = 1_000_000_007;
+        int pInv = BigInteger.valueOf(p).modInverse(BigInteger.valueOf(MOD)).intValue();
+
+        long bHash = 0, power = 1;
+        for (int i = 0; i < B.length(); i++) {
+            bHash += power * B.codePointAt(i);
+            bHash %= MOD;
+            power = (power * p) % MOD;
+        }
+
+        long aHash = 0; power = 1;
+        for (int i = 0; i < B.length(); i++) {
+            aHash += power * A.codePointAt(i % A.length());
+            aHash %= MOD;
+            power = (power * p) % MOD;
+        }
+
+        if (aHash == bHash && check(0, A, B)) return q;
+        power = (power * pInv) % MOD;
+
+        for (int i = B.length(); i < (q + 1) * A.length(); i++) {
+            aHash -= A.codePointAt((i - B.length()) % A.length());
+            aHash *= pInv;
+            aHash += power * A.codePointAt(i % A.length());
+            aHash %= MOD;
+            if (aHash == bHash && check(i - B.length() + 1, A, B)) {
+                return i < q * A.length() ? q : q + 1;
+            }
+        }
+        return -1;
+    }
+}
