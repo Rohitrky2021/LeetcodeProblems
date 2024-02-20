f13beaed03c73be615cc0aefddaa1387bc56c032
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-20 17:17:37 +0530

    Time: 1 ms (76.87%) | Memory: 45.4 MB (67.63%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 
-class Solution {
+// M1 using Set 
+class Solution1 {
     public void setZeroes(int[][] mat) {
         HashSet<Integer> row = new HashSet<>();
         HashSet<Integer> col = new HashSet<>();
@@ -23,6 +24,49 @@ class Solution {
         for (int y : col) {
             for (int x = 0; x < mat.length; x++) {
                 mat[x][y] = 0;
+            }
+        }
+    }
+}
+
+
+class Solution {
+
+    public void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean firstRow = false;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    if (i == 0) {
+                        firstRow = true;
+                    } else {
+                        matrix[i][0] = 0;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (firstRow) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
             }
         }
     }
