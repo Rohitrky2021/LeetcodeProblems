1bd89369ee33a3bc007085117862a726f73fcc2f
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-21 10:50:54 +0530

    Time: 3 ms (100.00%) | Memory: 44.2 MB (13.17%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
@@ -1,4 +1,15 @@
-class Solution {
+
+// Approach 1
+// Description: Iterate through the range [left, right]. This approach will give Time Limit Exceeded (TLE) for large ranges.
+// Time Complexity: O(right - left)
+// Space Complexity: O(1)
+
+// Approach 2
+// Description: Shift both left and right to the right until >> left == right <<< , while counting how many times you shifted.
+// Time Complexity: O(log2(left) + log2(right))
+// Space Complexity: O(1)
+
+class Solution1 {
     public int rangeBitwiseAnd(int m, int n) {
         int shift = 0;
         
@@ -11,5 +22,19 @@ class Solution {
         
         // Left shift back to obtain the result
         return m << shift;
+    }
+}
+
+
+// Approach 3
+// Description: Set the rightmost 1 bit of right to 0 until left >= right.
+// Time Complexity: O(popcount(right))
+// Space Complexity: O(1)
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        while (left < right) {
+            right &= (right - 1); // Set the rightmost 1 bit of right to 0
+        }
+        return right;
     }
 }
