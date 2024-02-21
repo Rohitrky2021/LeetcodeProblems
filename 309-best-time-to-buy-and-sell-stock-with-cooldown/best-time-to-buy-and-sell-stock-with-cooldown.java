a5e89525e9e04e91fc691487fb83ba3a919435e7
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-22 02:45:48 +0530

    Time: 1 ms (80.67%) | Memory: 42.3 MB (14.02%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution2 {
     public int maxProfit(int[] prices) {
          Map<String, Integer> cache = new HashMap<>();
         return dfs(0, true, prices, cache);
@@ -32,6 +32,46 @@ class Solution {
 
         int best = Math.max(nothing, action);
         cache.put(key, best);
+        return best;
+    }
+}
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n + 1][2];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = -1;
+            dp[i][1] = -1;
+        }
+        return dfs(0, 1, prices, dp);
+    }
+
+    private int dfs(int currIdx, int canBuy, int[] prices, int[][] dp) {
+        if (currIdx >= prices.length) {
+            return 0;
+        }
+        
+        if (dp[currIdx][canBuy] != -1) {
+            return dp[currIdx][canBuy];
+        }
+
+        // do nothing
+        int nothing = dfs(currIdx + 1, canBuy, prices, dp);
+
+        // buy
+        int action;
+        if (canBuy == 1) {
+            action = dfs(currIdx + 1, 0, prices, dp) - prices[currIdx];
+        }
+        // sell
+        else {
+            action = dfs(currIdx + 2, 1, prices, dp) + prices[currIdx];
+        }
+
+        int best = Math.max(nothing, action);
+        dp[currIdx][canBuy] = best;
         return best;
     }
 }
