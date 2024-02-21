1139661c072d966fa007eb1ea7ce6df3e35bb399
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-22 00:40:40 +0530

    Time: 22 ms (62.23%) | Memory: 48.7 MB (16.03%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,63 +1,47 @@
 class Solution {
     public boolean isMatch(String s, String p) {
+        
+        int n = s.length();
+        int m = p.length();
 
-        // M1 
-        // return tabu(  s,  p, s.length(), p.length());
+        int[][] dp = new int[n][m];
+        for(int[] arr : dp){
+            Arrays.fill(arr, -1);
+        }
 
-    //  M2
-        boolean dp[][] = new boolean[s.length() + 1][p.length() + 1];
-        dp[0][0] = true;
-        for (int i = 1; i < dp.length; i++) {
-            dp[i][0] = false;
-        }
-        for (int j = 1; j < dp[0].length; j++) {
-            if (p.charAt(j-1) == '*') {
-                dp[0][j] = dp[0][j - 1];
-            }else{
-                dp[0][j] = false;
-            }
-        }
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[0].length; j++) {
-                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
-                    dp[i][j] = dp[i-1][j-1];
-                } else if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
-                } else {
-                    dp[i][j] = false;
-                }
-            }
-        }
-        // printDp(dp);
-        return dp[s.length()][p.length()];
+        return memo(s,p,n-1,m-1,dp);
     }
- 
 
-        public static boolean tabu(String s, String p, int n, int m){
-        boolean[][] dp = new boolean[n+1][m+1];
-        dp[0][0] = true; // 1
-        for(int i = 1; i<=n; i++){ // 2
-            dp[i][0] = false;
-        }
-        for(int j = 1; j<=m; j++){ // 3
-            if(p.charAt(j-1) != '*'){
-                break;
-            }
-            dp[0][j] = true;
+     public static boolean memo(String s, String p, int i, int j, int[][] dp){
+
+        if(i < 0 && j < 0){
+            return true;
         }
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
-                    dp[i][j] = dp[i-1][j-1];
-                }else if(p.charAt(j-1) == '*'){
-                    dp[i][j] = dp[i][j-1] | dp[i-1][j] | dp[i-1][j-1];
-                }
-                else {
-                    dp[i][j] = false;
-                }
+        if( i < 0 && j>= 0){
+            for(int x=0; x<=j;x++){
+                if(p.charAt(x) != '*') return false;
             }
+            return true;
         }
-        return dp[n][m];
+        else if(j < 0 && i>=0 ){  // because in j i.e p is the only pattern Available 
+            return false;
+        }
+
+        if(dp[i][j] != -1) return dp[i][j] == 0 ? false : true;
+
+        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
+            boolean val = memo(s,p,i-1,j-1,dp);
+            dp[i][j] = val == true ? 1 : 0; // store kr lena then ans neeche return kr dena 
+            return val;
+        }else if(p.charAt(j) == '*'){
+            boolean val =  memo(s,p,i,j-1,dp) | memo(s,p, i-1, j,dp) | memo(s,p,i-1,j-1,dp);
+            dp[i][j] = val == true ? 1 : 0;
+            return val;
+        }
+        else {
+            dp[i][j] = 0;
+            return false;
+        }
     }
 }
