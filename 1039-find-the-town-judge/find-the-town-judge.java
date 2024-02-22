64e727d13c3763742d41e80424a6099c47ad2748
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-22 14:39:27 +0530

    Time: 2 ms (99.85%) | Memory: 53.3 MB (72.22%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,4 +1,5 @@
-class Solution {
+class Solution1 {
+    // Same as Celebrity Problem 
     public int findJudge(int n, int[][] t) {
         // int n=t.length;
         int hash[]=new int[n];
@@ -18,5 +19,23 @@ class Solution {
 
        return possible; 
         
+    }
+}
+
+//  SImply a graph whoes outDegree is Simply 0
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if (trust.length == 0 && n == 1) 
+            return 1;
+        int[] count = new int[n + 1];
+        for (int[] person : trust) {
+            count[person[0]]--;
+            count[person[1]]++;
+        }
+
+        for (int person = 0; person < count.length; person++) {
+            if (count[person] == n - 1) return person;
+        }
+        return -1;
     }
 }
