1f2e1354a4168a1cfd7ab85822abe6047e02eebe
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-02-23 17:21:16 +0530

    Time: 24 ms (43.78%) | Memory: 44 MB (15.59%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,6 +1,7 @@
 class Solution {
     public int countPrimeSetBits(int left, int right) {
         int count = 0;
+        isPrime(right+1);
         for (int i = left; i <= right; i++) {
             if (isPrimeSetBits(i)) {
                 count++;
@@ -11,18 +12,29 @@ class Solution {
 
     public boolean isPrimeSetBits(int num) {
         int bitCount = Integer.bitCount(num);
-        return bitCount > 1 && isPrime(bitCount);
+        return bitCount > 1 && ck[bitCount];
     }
+    public boolean ck[];
 
-    public boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                return false;
+    public void isPrime(int high) {
+        // boolean ans[]=new boolean[high + 1];
+        ck = new boolean[high + 1];
+        Arrays.fill(ck, true);
+        ck[1] = false;
+        ck[0] = false;
+
+        for (int i = 2; (i * i) <= high; i++) {
+            if (ck[i] == true) {
+                for (int j = i * i; j <= Math.sqrt(high); j = j + i) {// Stroring in Ck[] --> SqrtN
+                    ck[j] = false;
+                }
             }
         }
-        return true;
+        // for (int i = 2; i * i <= high; i++) {
+        //     if (ck[i] == true) {
+        //         // cout<< i<<"\n";
+        //        ans[i-2]=true;
+        //     }
+        // }
     }
 }
