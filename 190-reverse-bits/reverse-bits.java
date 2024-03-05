bf50d6286d276dafb5443ce7d09e27a68312cf94
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-03-05 11:03:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (17.63%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -4,10 +4,37 @@ public class Solution {
     public int reverseBits(int n) {
         int reversed = 0;
         for (int i = 0; i < 32; i++) {
-            reversed <<= 1; // Left shift the result to make space for the next bit
+            reversed=reversed<<1; // Left shift the result to make space for the next bit
             reversed |= n & 1; // Add the least significant bit of 'n' to the result
             n >>= 1; // Right shift 'n' to process the next bit
         }
         return reversed;
     }
 }
+
+
+ class Solution1 {
+
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        return ans;
+    }
+}
+
+
+//    int ans = 0;
+//     for(int i=0;i<32;i++){
+//         int lsb = n & 1 ;
+//         int temp= lsb<<(31-i);
+//         ans = ans|temp;
+//         n = n>>1;
+//     }
+//     return ans;
+// }
