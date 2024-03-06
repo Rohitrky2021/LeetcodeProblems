e8d0f7959fd1b9b85bcc60ac790ca0d62b7782d7
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-03-06 09:38:02 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (87.92%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -1,17 +1,10 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+ 
+
+
+// M1  Using Floyd 
 public class Solution {
     public boolean hasCycle(ListNode head) {
-         if(head==null)return false;
+        if(head==null)return false;
         ListNode s=head;
         ListNode f=head.next;
         while(s!=f){
@@ -23,4 +16,35 @@ public class Solution {
     }
     return true;
 }
+}
+
+
+// M2  Using Hashset 
+
+  class Solution2 {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> visited = new HashSet<>();
+        while (head != null) {
+            if (!visited.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+} 
+
+// M-3 
+//since constraints for value are -10^5 <= Node.val <= 10^5
+//then I use 1_000_000 as indicator that this node has been
+//visited
+  class Solution3 {
+    public boolean hasCycle(ListNode head) {
+        while (head != null) {
+            if (head.val == 1_000_000) return true;
+            head.val = 1_000_000;
+            head = head.next;
+        }
+        return false;
+    }
 }
