dc9f9e09112c3cffd73dcc06fdb0fe8ac6fbdec7
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-03-08 08:12:50 +0530

    Time: 1 ms (99.64%) | Memory: 42 MB (88.71%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -6,11 +6,10 @@ class Solution {
 
         for(int x:nums){
             fre[x]++;
-
             max=Math.max(max,fre[x]);
         }
 
-        
+        //  int max=Arrays.stream(fre).max().orElse(0); ---> This was taking More time now shifted this to above 
 
         int count=0;
 
