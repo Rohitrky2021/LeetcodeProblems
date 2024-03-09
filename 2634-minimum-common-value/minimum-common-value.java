1f98abc7138cbc9d2ec92762cca6af817824dbc5
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-03-09 09:50:24 +0530

    Time: 0 ms (100.00%) | Memory: 62.5 MB (69.04%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,4 +1,5 @@
-class Solution {
+//  M1 Usign Hashing
+class Solution1 {
     public int getCommon(int[] nums1, int[] nums2) {
 
         HashSet<Integer> s=new HashSet<>();
@@ -15,5 +16,25 @@ class Solution {
 
         return -1;
  
+    }
+}
+
+// M2 Using 2 Pointers
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        if (nums1[nums1.length-1] < nums2[0] || nums2[nums2.length-1] < nums1[0]) {
+            return -1;
+        }
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                return nums1[i];
+            }
+        }
+        return -1;
     }
 }
