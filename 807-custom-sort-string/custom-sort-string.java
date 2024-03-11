aa98c4b553f6423b44294dc75c2c5264eddcd89f
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-03-11 15:57:50 +0530

    Time: 3 ms (36.64%) | Memory: 41.2 MB (92.79%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,33 +1,17 @@
 import java.util.HashMap;
 
 class Solution {
-
-    class Pair{
-        int k,v;
-
-        public Pair (int k,int v){
-            this.k=k;
-            this.v=v;
-        }
-    }
     public String customSortString(String order, String s) {
-        int[] freq = new int[26]; // Fixed declaration of integer array
+         
         StringBuilder ans = new StringBuilder(""); // Fixed StringBuilder declaration
 
-        int i = 0;
-        for (char x : order.toCharArray()) { // Fixed iteration over order string
-            freq[x - 'a'] = i;
-            i++;
-        }
-
-        HashMap<Character, Pair> hs = new HashMap<>();
+        HashMap<Character, Integer> hs = new HashMap<>();
 
         for (char x : s.toCharArray()) {
-            if(hs.containsKey(x)) {
-                Pair pair = hs.get(x);
-                hs.put(x, new Pair(pair.k, pair.v + 1)); // Incrementing the count for existing character
+            if (hs.containsKey(x)) {
+                hs.put(x, hs.get(x) + 1); // Incrementing the count for existing character
             } else {
-                hs.put(x, new Pair(freq[x - 'a'], 1)); // Adding new character to the HashMap
+                hs.put(x, 1); // Adding new character to the HashMap
             }
         }
 
@@ -35,9 +19,9 @@ class Solution {
         StringBuilder sortedString = new StringBuilder();
         for (char c : order.toCharArray()) {
             if (hs.containsKey(c)) {
-                while(hs.get(c).v != 0) {
+                while (hs.get(c) > 0) {
                     sortedString.append(c);
-                    hs.put(c, new Pair(hs.get(c).k, hs.get(c).v - 1));
+                    hs.put(c, hs.get(c) - 1);
                 }
                 hs.remove(c); // Removing the character after adding to the sorted string
             }
@@ -45,9 +29,9 @@ class Solution {
 
         // Append the remaining characters (if any) from s which are not present in the custom order
         for (char key : hs.keySet()) {
-            while(hs.get(key).v != 0) {
+            while (hs.get(key) > 0) {
                 sortedString.append(key);
-                hs.put(key, new Pair(hs.get(key).k, hs.get(key).v - 1));
+                hs.put(key, hs.get(key) - 1);
             }
         }
 
