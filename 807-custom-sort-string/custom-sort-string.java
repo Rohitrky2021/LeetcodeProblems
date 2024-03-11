7ea1f8f4840b1ee0dd2206e9498b8f3a0a8d52e8
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-03-11 18:29:10 +0530

    Time: 5 ms (16.60%) | Memory: 41.9 MB (15.58%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-class Solution {
+class Solution1 {
     public String customSortString(String order, String s) {
         int[] arr = new int[26];
         for (char ch : s.toCharArray()) {
@@ -24,5 +24,29 @@ class Solution {
         }
 
         return sb.toString();
+    }
+}
+
+class Solution {
+    public String customSortString(String order, String s) {
+        // Create char array for editing
+        int N = s.length();
+        Character[] result = new Character[N];
+        for (int i = 0; i < N; i++) {
+            result[i] = s.charAt(i);
+        }
+
+        // Define the custom comparator
+        Arrays.sort(result, (c1, c2) -> {
+            // The index of the character in order determines the value to be sorted by
+            return order.indexOf(c1) - order.indexOf(c2);
+        });
+
+        // Return the result
+        String resultString = "";
+        for (Character c: result) {
+            resultString += c;
+        }
+        return resultString;
     }
 }
