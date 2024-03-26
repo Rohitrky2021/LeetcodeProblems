d1be1f50cebe304697a367dfa394c0bcc5cc3e8c
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-03-26 10:11:26 +0530

    Time: 1 ms (98.00%) | Memory: 54.5 MB (95.48%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-class Solution {
+class Solution2 {
     public int firstMissingPositive(int[] arr) {
         cycleSort(arr, arr.length);
         int i;
@@ -76,4 +76,23 @@ class Solution1 {
 
         return nums.length + 1;
     }
+}
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        boolean[] found = new boolean[n+1];
+        for(int i = 0;i< n; i++){
+            if (nums[i]>0 && nums[i]<= n){
+                found[nums[i]]=true;
+
+            }
+        }
+        for(int i= 1;i<=n;i++){
+            if(!found[i]){
+                return i;
+            }
+        }
+        return n+1;
+ }
 }
