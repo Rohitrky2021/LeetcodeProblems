4176fe602aca21b172089fd89524a442cef99627
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-05-10 11:31:53 +0530

    Time: 362 ms (57.20%) | Memory: 79.9 MB (32.75%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
-class Solution {
+// Approach: Brute Force (Min Heap)
+// Time complexity: O(n * n * logk)
+// Space complexity: O(k)
+
+class Solution1 {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
         int n = arr.length;
         if (n == 2) return new int[]{arr[0], arr[1]};
@@ -18,5 +22,80 @@ class Solution {
         }
 
         return pq.poll();
+    }
+}
+
+// Approach: Brute Force (Max Heap)
+// Complexity
+// Time complexity: O(n * n)
+// Space complexity: O(n)
+
+
+
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        PriorityQueue<double[]> maxHeap = new PriorityQueue<>(k, (a, b) -> Double.compare(b[0], a[0]));
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                maxHeap.offer(new double[]{(double) arr[i] / arr[j], arr[i], arr[j]});
+
+                if (maxHeap.size() > k) {
+                    maxHeap.poll();
+                }
+            }
+        }
+
+        double[] res = maxHeap.poll();
+        return new int[]{(int) res[1], (int) res[2]};
+    }
+}
+
+
+
+// Approach: Best (Binary Search)
+// Complexity
+// Time complexity: O(nlogn)
+
+// Space complexity: O(1)
+class Solution2 {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        int n = arr.length;
+        double left = 0, right = 1, mid;
+        int[] res = new int[2];
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            int j = 1, total = 0, num = 0, den = 0;
+            double maxFrac = 0;
+            for (int i = 0; i < n; ++i) {
+                while (j < n && arr[i] >= arr[j] * mid) {
+                    ++j;
+                }
+                
+                total += n - j;
+
+                if (j < n && maxFrac < arr[i] * 1.0 / arr[j]) {
+                    maxFrac = arr[i] * 1.0 / arr[j];
+                    num = i;
+                    den = j;
+                }
+            }
+
+            if (total == k) {
+                res[0] = arr[num];
+                res[1] = arr[den];
+                break;
+            }
+
+            if (total > k) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        return res;
     }
 }
