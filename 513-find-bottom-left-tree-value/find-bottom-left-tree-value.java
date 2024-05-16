7f5e411e3c907a4eb6cc2978c14b8f77b9f2520b
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-05-16 07:16:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (89.10%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -13,6 +13,21 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     int ans = 0;
     int maxDepth = -1;
@@ -32,6 +47,7 @@ class Solution {
         }
 
         traverse(node.left, depth + 1);
+        
         traverse(node.right, depth + 1);
     }
 }
