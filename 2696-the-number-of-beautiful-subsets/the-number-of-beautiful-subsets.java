acad15d81ac96d4ca208977084d642220cd81986
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-05-23 15:49:23 +0530

    Time: 394 ms (57.07%) | Memory: 44.9 MB (42.58%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int dfs(int[] nums, int idx, int k, HashMap<Integer, Integer> mp) {
         if (idx == nums.length) return 1;
 
@@ -22,4 +22,27 @@ class Solution {
         int ans = dfs(nums, 0, k, mp);
         return ans - 1;
     }
+}
+
+class Solution {
+  private int dfs(int[] nums, int k, int i, Set<Integer> set) {
+    if (i == nums.length)
+     return set.isEmpty() ? 0 : 1;
+
+    var cnt = dfs(nums, k, i+1, set);
+    
+    if (set.contains(nums[i] - k)) return cnt;
+
+    set.add(nums[i]);
+    cnt += dfs(nums, k, i+1 ,set);
+    set.remove(nums[i]);
+
+    return cnt;
+  }
+
+  public int beautifulSubsets(int[] nums, int k) {
+    Arrays.sort(nums);
+
+    return dfs(nums, k, 0, new HashSet<Integer>());
+  }
 }
