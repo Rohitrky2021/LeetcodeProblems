8125667daad157c8c3da1dee0eb91323d781d5ed
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-05-28 10:50:36 +0530

    Time: 5 ms (97.52%) | Memory: 43.6 MB (12.42%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
@@ -22,3 +22,39 @@ class Solution {
         return ans;
     }
 }
+
+//  Using Binar Search 
+
+class Solution1 {
+    public int equalSubstring(String s, String t, int maxCost) {
+        // Calculate the prefix sum to get the cost of `s.substring(i, length)`
+        int n = s.length();
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + Math.abs(s.charAt(i) - t.charAt(i));
+        }
+
+        // Binary search the maximum length
+        int l = 0, r = n;
+        while (l < r) {
+            int m = l + (r - l + 1) / 2;
+            if (check(prefix, maxCost, m)) {
+                l = m;
+            } else {
+                r = m - 1;
+            }
+        }
+        return l;
+    }
+
+    // Check if there exists a substring that meets the condition
+    private boolean check(int[] prefix, int maxCost, int length) {
+        for (int i = 0; (i + length) <= prefix.length; i++) {
+            int cost = prefix[i + length] - prefix[i];
+            if (cost <= maxCost) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
