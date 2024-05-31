da72a16e534d083d347f415736175199c65afd00
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-05-31 16:08:58 +0530

    Time: 3 ms (68.17%) | Memory: 45.2 MB (71.24%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,44 @@
+class Solution1 {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        if( nums.length==1 || nums[0]!=nums[1] ){
+            return nums[0];
+        }
+          if(nums[nums.length-1]!=nums[nums.length-2]){
+            return nums[nums.length-1];
+        }
+
+        for(int i=1;i<nums.length-1;i++){
+            if(nums[i-1]!=nums[i] && nums[i+1]!=nums[i]  ){
+            return nums[i];
+            // break;
+            }
+        }
+        return 0; 
+    }
+}
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0;i<32;i++){
+            int ones=0,zero=0;
+            int temp=1<<(i);
+
+            for(int num:nums){
+                if((num & temp)==0)zero++;
+                else ones++;
+            }
+            if(ones%3!=0){
+                ans|=temp;
+            }
+        }
+
+        return ans;
+    }
+}
+
+
+
+
