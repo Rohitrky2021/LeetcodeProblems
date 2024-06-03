164f3b088bb1768de2126026abed34eefac2b915
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-06-03 10:23:30 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (65.34%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+         int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        // Transpose the matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = i; j < cols; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Flip the matrix diagonally
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][cols - 1 - j];
+                matrix[i][cols - 1 - j] = temp;
+            }
+        }
+    }
+}
