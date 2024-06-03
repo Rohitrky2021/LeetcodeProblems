a83fdfc364d36dd4f9ea2eb48101f7789bfc8674
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-06-03 21:02:20 +0530

    Time: 755 ms (24.39%) | Memory: 117.4 MB (6.24%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.java
@@ -0,0 +1,56 @@
+class Solution {
+
+    int check=0;
+    public String getPermutation(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        check=k;
+        int nums[]=new int[n];
+        for(int i=1;i<=n;i++){
+            nums[i-1]=i;
+        }
+
+        ans=permute(nums);
+
+        String st="";
+
+        for(int x:ans.get(k-1)){
+            st+=x;
+        }
+
+        return st ;
+
+    }
+
+    public  List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        backtrack(nums, visited, current, result);
+        return result;
+    }
+
+
+    private void backtrack(int[] nums, boolean[] visited, List<Integer> current, List<List<Integer>> result) {
+
+        if(result.size()>check)return;
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            //  if (visited[i]) {
+            //     continue;
+            //     // ---------------->when revise ones uncomment and run this !
+            // }
+            visited[i] = true;
+            current.add(nums[i]);
+            backtrack(nums, visited, current, result);
+            current.remove(current.size() - 1);
+            visited[i] = false;
+        }
+    }
+}
