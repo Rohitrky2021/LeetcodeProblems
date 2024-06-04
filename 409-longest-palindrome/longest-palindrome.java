bb0313533290a0a5c337931702c3b6282c77699e
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-06-04 22:31:36 +0530

    Time: 1 ms (100.00%) | Memory: 41.6 MB (59.67%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,10 +1,31 @@
-class Solution {
+class Solution2 {
     public int longestPalindrome(String st) {
         int[] frequency = new int[70]; // Assuming ASCII characters
 
         for (char c : st.toCharArray()) {
             System.out.println((int)c);
             frequency[c-'A']++;
+        }
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int freq : frequency) {
+            evenCount += freq / 2;
+            oddCount += freq % 2;
+        }
+
+        return 2 * evenCount + (oddCount > 0 ? 1 : 0);
+    }
+}
+
+
+class Solution {
+    public int longestPalindrome(String st) {
+        int[] frequency = new int[128]; // Assuming ASCII characters
+
+        for (char c : st.toCharArray()) {
+            frequency[c]++;
         }
 
         int evenCount = 0;
