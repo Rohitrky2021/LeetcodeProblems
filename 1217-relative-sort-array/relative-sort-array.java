d505f7eb63b64469fa872d50568fb3eac970dc62
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-06-11 07:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (91.90%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+
+        int freq[]=new int[1005];
+
+        for(int i=0;i<arr1.length;i++){
+            freq[arr1[i]]++;
+        }
+
+        int j=0;
+        for(int i=0;i<arr2.length;i++){
+            if(freq[arr2[i]]!=0){
+                while(freq[arr2[i]]!=0){
+                    arr1[j++]=arr2[i];
+                    freq[arr2[i]]--;
+                }
+            }
+        }
+
+         for(int i=0;i<1001;i++){
+            if(freq[i]!=0){
+                while(freq[i]!=0){
+                    arr1[j++]=i;
+                    freq[i]--;
+
+                }
+            }
+        }
+
+        return arr1;
+        
+    }
+}
