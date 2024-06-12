2e9d77fae8b709a4978a5ee791311c2bda1a5b22
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-06-12 12:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (77.63%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,28 @@
 class Solution {
-    public void sortColors(int[] nums) {
-         Arrays.sort(nums);
+    public void sortColors(int[] arr) {
+        int n = arr.length;
+        int zero = 0;
+        int two = n - 1;
+
+        for (int i = 0; i <= two; ) {
+            if (arr[i] == 2) {
+                // Swap arr[i] with arr[two]
+                int temp = arr[i];
+                arr[i] = arr[two];
+                arr[two] = temp;
+                two--;
+                // Do not increment i here because the swapped element needs to be checked
+            } else if (arr[i] == 0) {
+                // Swap arr[i] with arr[zero]
+                int temp = arr[i];
+                arr[i] = arr[zero];
+                arr[zero] = temp;
+                zero++;
+                i++;
+            } else {
+                // If it's 1, just move to the next element
+                i++;
+            }
+        }
     }
 }
