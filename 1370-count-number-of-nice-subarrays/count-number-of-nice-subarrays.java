cbae53ebe4deabe7df3245edb0002ba6b8e272d8
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-06-22 10:50:03 +0530

    Time: 10 ms (80.88%) | Memory: 54.7 MB (71.73%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int numberOfSubarrays(int[] nums, int k) {
         int c = 0; // count of odd numbers
         int tail = 0, ans = 0, sum = 0;
@@ -19,11 +19,80 @@ class Solution {
     }
 }
 
-class Solution1 {   /// THis is Not working at aall later if u come again try 
+class Solution2 {
+
+    public int numberOfSubarrays(int[] nums, int k) {
+        int currSum = 0, subarrays = 0;
+        Map<Integer, Integer> prefixSum = new HashMap<>();
+        prefixSum.put(currSum, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i] % 2;
+            // Find subarrays with sum k ending at i
+            if (prefixSum.containsKey(currSum - k)) {
+                subarrays = subarrays + prefixSum.get(currSum - k);
+            }
+            // Increment the current prefix sum in hashmap
+            prefixSum.put(currSum, prefixSum.getOrDefault(currSum, 0) + 1);
+        }
+
+        return subarrays;
+    }
+}
+
+class Solution4 {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int[] map = new int[50001];
+        int sum = 0;
+        map[0] = 1;
+        int count = 0;
+        int goal = k;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i] % 2;
+            if (sum >= goal) {
+                count += map[sum - goal];
+            }
+            map[sum]++;
+        }
+        return count;
+    }
+}
+
+class Solution {
+    public int helpMe(int[] nums, int goal) {
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int cnt = 0;
+        if (goal < 0) {
+            return 0;
+        }
+        while (r < nums.length) {
+            sum += nums[r] % 2; ////// <------
+            while (sum > goal) {
+                if (nums[l] % 2 == 1) { //// <-------
+                    sum--;
+                }
+                l++;
+            }
+            cnt = cnt + r - l + 1;
+            r++;
+        }
+        return cnt;
+    }
+
+    public int numberOfSubarrays(int[] nums, int k) {
+
+        return helpMe(nums, k) - helpMe(nums, k - 1);
+    }
+
+}
+
+class Solution3 { /// THis is Not working at aall later if u come again try
     public int numberOfSubarrays(int[] nums, int k) {
         int c = 0; // count of odd numbers
         int tail = 0, ans = 0;
-       
 
         for (int head = 0; head < nums.length; head++) {
             if (nums[head] % 2 != 0) {
@@ -38,9 +107,9 @@ class Solution1 {   /// THis is Not working at aall later if u come again try
             }
 
             if (c == k) {
-                ans ++;
+                ans++;
             }
- 
+
         }
 
         return ans;
