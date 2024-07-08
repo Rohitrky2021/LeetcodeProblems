c062f6b26115539e2e6d40d145b5ea42966b405e
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-07-08 20:48:08 +0530

    Time: 68 ms (5.01%) | Memory: 40.5 MB (58.65%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -0,0 +1,40 @@
+ class Solution {
+    public int findTheWinner(int n, int k) {
+        if(k==1)return n;
+        int freq[] = new int[n];
+        int lasti = 0;
+        int check = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            freq[i] = 0;  // Initialize freq array with 0
+        }
+
+        while (check != n - 1) {
+            count = 0;
+
+            while (count < k) {
+                if (freq[lasti] == 0) {
+                    count++;
+                }
+                if(count==k) freq[lasti]++;
+                lasti = (lasti + 1) % n;
+            }
+
+            check++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (freq[i] == 0) {
+                return i+1 ;
+            }
+        }
+
+        return -1;  // In case something goes wrong
+    }
+}
+
+// 1 2 3 4 5
+// 2 4 6 8 10
+
+// 2 4 1 3 5
