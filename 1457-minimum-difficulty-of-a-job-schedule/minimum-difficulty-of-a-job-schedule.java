9092b6ad2c3d6ee531a6888ba62315bcc7aa1cb2
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-07-10 07:49:07 +0530

    Time: 30 ms (25.60%) | Memory: 41.8 MB (29.95%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
@@ -22,12 +22,74 @@ class Solution {
 
         for (int i = idx; i <= arr.length - k; i++) {
             max = Math.max(max, arr[i]);
-            int nextDifficulty = help(arr, k - 1, i + 1, memo);
+            int nextDifficulty =  max + help(arr, k - 1, i + 1, memo); // mENd my this day & going to the another day 
             if (nextDifficulty != -1)
-                minDifficulty = Math.min(minDifficulty, max + nextDifficulty);
+                minDifficulty = Math.min(minDifficulty,nextDifficulty);
         }
 
         memo[idx][k] = (minDifficulty == Integer.MAX_VALUE) ? -1 : minDifficulty;
         return memo[idx][k];
+    }
+}
+
+
+class Solution1 {  // Recurrsion only soluiton so will give TLE 
+    public int minDifficulty(int[] jobDifficulty, int d) {
+        if (jobDifficulty.length < d) {
+            return -1;
+        }
+        int len = jobDifficulty.length;
+        int sum = 0;
+        for (int i = 0; i < jobDifficulty.length; i++) {
+            sum += jobDifficulty[i];
+        }
+        if (sum == 0) {
+            return 0;
+        }
+        return helper(jobDifficulty, d, 0);
+    }
+
+    private int helper(int[] jd, int daysLeft, int idx) {
+        int len = jd.length;
+        if (daysLeft == 1) {
+            int num = 0;
+            for (int i = idx; i < len; i++) {
+                num = Math.max(num, jd[i]);
+            }
+            return num;
+        }
+
+        int max = jd[idx];
+        daysLeft--;
+        int stop = len - idx - daysLeft + 1;
+
+        int res = Integer.MAX_VALUE;
+        for (int i = 1; i < stop; i++) {
+            max = Math.max(max, jd[idx + i - 1]);
+            int ans = max +  helper(jd, daysLeft, idx + i); // , idx + i --> starting ccheck for new day from idx in new looop
+            res = Math.min(res, ans);
+        }
+        return res;
+    }
+}
+
+class Solution2 {
+    public int minDifficulty(int[] jobDiff, int d) {
+        if(d > jobDiff.length) return -1;
+        int res = diff(0, jobDiff, d);
+        if(res == Integer.MAX_VALUE) return -1;
+        return res;
+    }
+    private int diff(int curr, int jobDiff[], int d) {
+        if(curr >= jobDiff.length && d <= 0) return 0;
+        if(curr >= jobDiff.length || d <= 0) return Integer.MAX_VALUE;
+        int maxDiff = -1;
+        int res = Integer.MAX_VALUE;
+        for(int i = curr; i<jobDiff.length; i++) {  // es loop k liye andar waali saari cheeze valid rhenge 
+            maxDiff = Math.max(maxDiff, jobDiff[i]);
+            int temp =  diff(i+1, jobDiff, d-1);
+            if(temp != Integer.MAX_VALUE) res = Math.min(res,maxDiff + temp);
+        }
+        return res;
     }
 }
