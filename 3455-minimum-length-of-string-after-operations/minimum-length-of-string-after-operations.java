79b43179aa6b8399cca47e81ed6b6d2c6118c9bd
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-07-21 00:40:09 +0530

    Time: 85 ms (100.00%) | Memory: 46.2 MB (100.00%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int minimumLength(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        int n = s.length();
+        
+        if (right + 1 < 3) return s.length();
+        
+        HashMap<Character, Integer> hs = new HashMap<>();
+        
+        int ans = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            hs.put(c, hs.getOrDefault(c, 0) + 1);
+        }
+        
+        for (Map.Entry<Character, Integer> entry : hs.entrySet()) {
+            ans += entry.getValue() % 2==0?2:1;
+        }
+        
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        System.out.println(solution.minimumLength("abaacbcbb")); // Expected Output: 5
+        System.out.println(solution.minimumLength("aa")); // Expected Output: 2
+    }
+}
