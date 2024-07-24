4cd4f321d8afbfa5d2bae49f8b9a7850294b520d
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-07-24 08:50:07 +0530

    Time: 93 ms (77.45%) | Memory: 55.8 MB (34.31%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Solution {
+class Solution1 {
     public int[] sortJumbled(int[] map, int[] nums) {
         HashMap<Integer, Integer> hm = new HashMap<>();
         
@@ -22,5 +22,82 @@ class Solution {
         });
 
         return Arrays.stream(sortedNums).mapToInt(Integer::intValue).toArray();
+    }
+}
+
+
+ 
+class Solution2 {
+    public int[] sortJumbled(int[] map, int[] nums) {
+        // Map to store the original number and its transformed value
+        Map<Integer, Integer> transformedMap = new HashMap<>();
+
+        for (int num : nums) {
+            int transformedValue = 0;
+            String str = Integer.toString(num);
+            for (int i = 0; i < str.length(); i++) {
+                transformedValue = transformedValue * 10 + map[str.charAt(i) - '0'];
+            }
+
+            
+            transformedMap.put(num, transformedValue);
+            
+        }
+
+        // Create a TreeMap with a custom comparator that sorts by transformed value and then by original value
+        Map<Integer, Integer> sortedTransformedMap = new TreeMap<>((a, b) -> {
+            return  transformedMap.get(a)-(transformedMap.get(b));
+ 
+        });
+
+        // Put all entries from the transformed map to the sorted transformed map
+        sortedTransformedMap.putAll(transformedMap);
+
+        // Build the result array
+        int[] ans = new int[nums.length];
+        int index = 0;
+        for (Integer key : sortedTransformedMap.keySet()) {
+            ans[index++] = key;
+        }
+
+        return ans;
+    }
+}
+
+
+ 
+
+class Solution {
+    public int[] sortJumbled(int[] map, int[] nums) {
+        // List to store pairs of (original index, transformed value)
+        List<int[]> transformedList = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int transformedValue = 0;
+            String str = Integer.toString(num);
+            for (int j = 0; j < str.length(); j++) {
+                transformedValue = transformedValue * 10 + map[str.charAt(j) - '0'];
+            }
+            transformedList.add(new int[]{i, transformedValue});
+        }
+
+        // Sort the list based on transformed value and then by original index
+        transformedList.sort((a, b) -> {
+            int compare = Integer.compare(a[1], b[1]);
+            if (compare != 0) {
+                return compare;
+            } else {
+                return Integer.compare(a[0], b[0]); // Maintain original order if transformed values are the same
+            }
+        });
+
+        // Build the result array
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < transformedList.size(); i++) {
+            ans[i] = nums[transformedList.get(i)[0]];
+        }
+
+        return ans;
     }
 }
