362a939a4e722d10197453fcb42e23b64aeb6bd4
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-07-25 11:56:50 +0530

    Time: 3 ms (100.00%) | Memory: 56.8 MB (23.79%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -24,14 +24,16 @@ class Solution {
                 min = a[i];
             }
         }
-        int[] count = new int[max-min+1];
+        int[] count = new int[max-min+1];  
+        // Till now was just to get the length for the Array suing Min max 
+
         for(int v:a){
-            count[v-min]++;
+            count[v-min]++;  // making it 0 beased indx but making min as 1st number 
         }
         int k=0;
         for (int i = 0; i < count.length; i++) {
             while (count[i]>0){
-                a[k] = i+min;
+                a[k] = i+min; //again increase it to original 
                 k++;
                 count[i]--;
             }
