69295b151d4890332f3bcd9c979b0c598170bff6
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-07-27 00:11:07 +0530

    Time: 2 ms (62.74%) | Memory: 42.1 MB (33.29%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,29 +1,72 @@
-// class Solution {
-//     public boolean isMatch(String text, String pattern) {
-//         boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
-//         dp[text.length()][pattern.length()] = true;
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if(s == null || p == null) {
+            return false;
+        }
+        
+        int n = s.length();
+        int m = p.length();
 
-//         for (int i = text.length(); i >= 0; i--){
-//             for (int j = pattern.length() - 1; j >= 0; j--){
-//                 boolean first_match = (i < text.length() &&
-//                                        (pattern.charAt(j) == text.charAt(i) ||
-//                                         pattern.charAt(j) == '.'));
-//                 if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-//                     dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
-//                 } else {
-//                     dp[i][j] = first_match && dp[i+1][j+1];
-//                 }
-//             }
-//         }
-//         return dp[0][0];
-//     }
-// }
+        boolean dp[][] = new boolean[n+1][m+1];
+
+        dp[0][0] = true;
+        
+        // no need to initialize state[i][0] as false
+        // initialize state[0][j]
+        for(int j = 1; j < m+1; j++){
+            if(p.charAt(j-1) == '*'){
+                if (dp[0][j - 1] || (j > 1 && dp[0][j - 2])) {
+                    dp[0][j] = true;
+                }
+            }
+        }
+
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p.charAt(j-1) == '*'){
+                    //sure replacing by empty sequence needed case.
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        dp[i][j] = dp[i][j - 2];
+                    //not sure if replacing needed or not case.
+                    } else {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+                    }
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
+
+class Solution2 {
+    public boolean isMatch(String text, String pattern) {
+        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+        dp[text.length()][pattern.length()] = true;
+
+        for (int i = text.length(); i >= 0; i--){
+            for (int j = pattern.length() - 1; j >= 0; j--){
+                boolean first_match = (i < text.length() &&
+                                       (pattern.charAt(j) == text.charAt(i) ||
+                                        pattern.charAt(j) == '.'));
+                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+                } else {
+                    dp[i][j] = first_match && dp[i+1][j+1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
 
 enum Result {
     TRUE, FALSE
 }
 
-class Solution {
+class Solution3 {
     Result[][] memo;
 
     public boolean isMatch(String text, String pattern) {
