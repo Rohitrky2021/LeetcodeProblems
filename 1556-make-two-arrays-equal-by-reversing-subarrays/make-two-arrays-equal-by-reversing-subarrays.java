104226d24cac123f32e4b8e069f3f3ee1ae3ccef
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-08-03 16:07:02 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (86.16%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,10 +1,35 @@
-class Solution {
+class Solution1 {
     public boolean canBeEqual(int[] target, int[] arr) {
 
         Arrays.sort(target);
         Arrays.sort(arr);
 
         return Arrays.equals(target,arr);
+        
+    }
+}
+
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int n=arr.length;
+        int freq[]=new int[1000+2];
+
+        for(int x:target){
+            freq[x]++;
+        }
+
+        for(int x:arr){
+            if(freq[x]==0)return false;
+            else{
+                freq[x]--;
+            }
+
+
+        }
+        
+
+
+        return true;
         
     }
 }
