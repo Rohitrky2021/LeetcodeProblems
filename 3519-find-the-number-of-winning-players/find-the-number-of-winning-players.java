53300b1920612e42818cb8aa5f79a337c160997f
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-08-03 22:07:14 +0530

    Time: 3 ms (100.00%) | Memory: 45.3 MB (100.00%) - LeetSync

--- 3519-find-the-number-of-winning-players/find-the-number-of-winning-players.java
+++ 3519-find-the-number-of-winning-players/find-the-number-of-winning-players.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-class Solution {
+class Solution1 {
     public int winningPlayerCount(int n, int[][] pick) {
         // Sort the 2D array with a comparator
         Arrays.sort(pick, (a, b) -> {
@@ -29,4 +29,38 @@ class Solution {
 
         return count;
     }
+}
+ 
+class Solution {
+    public int winningPlayerCount(int n, int[][] pick) {
+        // Array to keep track of the counts of each color picked by each player
+        Map<Integer, Integer>[] playerPicks = new HashMap[n];
+        
+        // Initialize the array with HashMaps
+        for (int i = 0; i < n; i++) {
+            playerPicks[i] = new HashMap<>();
+        }
+
+        // Fill the array with the picks data
+        for (int[] p : pick) {
+            int player = p[0];
+            int color = p[1];
+            playerPicks[player].put(color, playerPicks[player].getOrDefault(color, 0) + 1);
+        }
+
+        // Count the number of winners
+        int winners = 0;
+        for (int i = 0; i < n; i++) {
+            for (int count : playerPicks[i].values()) {
+                if (count > i) {
+                    winners++;
+                    break; // No need to check further for this player
+                }
+            }
+        }
+
+        return winners;
+    }
+
+   
 }
