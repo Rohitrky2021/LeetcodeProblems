d4cac8f84491c457148750bc20bfe17b63864efe
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-08-03 14:52:08 +0530

    Time: 63 ms (48.66%) | Memory: 74.1 MB (64.06%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
@@ -4,6 +4,7 @@ class Solution {
         Arrays.sort(sortedPeople);
         
         Arrays.sort(flowers, (a, b) -> Arrays.compare(a, b));
+        
         Map<Integer, Integer> dic = new HashMap();
         PriorityQueue<Integer> heap = new PriorityQueue();
         
@@ -28,4 +29,30 @@ class Solution {
         
         return ans;
     }
+
+
+    public int countFlowers(int[] flowers, int k) {
+        if (flowers == null || flowers.length == 0 || k <= 0) {
+            return 0;
+        }
+        int n = flowers.length;
+        int[] days = new int[n];
+        for (int i = 0; i < n; i++) {
+            days[flowers[i] - 1] = i + 1;
+        }
+        int res = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (days[j] - days[i] > k) {
+                    break;
+                }
+                if (days[j] - days[i] == k) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
 }
+
+
