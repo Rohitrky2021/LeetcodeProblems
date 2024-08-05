7248ed17ddbfcbbf3f87d3ebcf2ee7ea93aebb4d
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-08-05 21:28:06 +0530

    Time: 10 ms (18.58%) | Memory: 49.7 MB (8.17%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        Integer[][] memo = new Integer[n][n];
+        return stoneGameHelper(piles, 0, n - 1, memo) > 0;
+    }
+
+    private int stoneGameHelper(int[] piles, int left, int right, Integer[][] memo) {
+        if (left > right) {
+            return 0;
+        }
+
+        if (memo[left][right] != null) {
+            return memo[left][right];
+        }
+
+        // Player's choice is either take the left or the right pile
+        int takeLeft = piles[left] - stoneGameHelper(piles, left + 1, right, memo);
+        int takeRight = piles[right] - stoneGameHelper(piles, left, right - 1, memo);
+
+        memo[left][right] = Math.max(takeLeft, takeRight);
+        return memo[left][right];
+    }
+}
