2c3aefe4b0fa16c65f5cccb1356451fccb2997ac
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-08-12 18:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (12.95%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,21 +1,37 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+ 
+
+//         Using Two Pointer Approach:
+//         Take a pointer second and put it at (n+1)th position from the beginning
+//         Take pointer first and move it forward till second reaches Last Node and second.next points to null
+//         At that point we would have reached the (n-1)th node from the end using the pointer first
+//         Unlink or Skip that node
+
 class Solution {
-    /**
-     * Removes the nth node from the end of the linked list.
-     * 
-     * @param head The head of the linked list.
-     * @param n    The position of the node to remove from the end.
-     * @return     The head of the updated linked list after removing the nth node from the end.
-     */
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null) return null;
+
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode first = temp, second = temp;
+
+        while (n > 0) {
+            second = second.next;
+            n--;
+        }
+
+        while (second.next != null) {
+            second = second.next;
+            first = first.next;
+        }
+
+        first.next = first.next.next;
+        return temp.next;
+    }
+}
+ 
+class Solution2 {
+  
     public ListNode removeNthFromEnd(ListNode head, int n) {
         // Check if the list is empty or has only one node
         if (head == null || head.next == null) return null;
@@ -45,6 +61,37 @@ class Solution {
             st.next = st.next.next;
         else
             st.next = null;
+
+        return head;
+    }
+}
+
+class Solution3 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null) return null;
+        int len=0,i=1;
+        ListNode  check=head,st=head;
+
+        while(check!=null){
+            check=check.next;
+            len++;
+        }
+        if(n-len+1==1)return head.next;
+
+        // From start it will be ( N- k+1){}
+
+        while(i!=len-n+1-1 && st.next!=null ){
+            st=st.next;
+            i++;
+        }
+
+        if(st.next!=null)
+                st.next=st.next.next;
+        else st.next=null;        
+        // while(i!=len-k+1-1){
+        //     st=st.next;
+        // }
+
 
         return head;
     }
