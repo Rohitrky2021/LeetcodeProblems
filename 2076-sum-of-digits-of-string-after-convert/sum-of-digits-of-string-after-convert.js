d8e164a96bb9d3ba177b3aba07f58c3e94e60f99
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-09-03 12:57:23 +0530

    Time: 46 ms (96.86%) | Memory: 50.5 MB (70.44%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var getLucky = function(s, k) {
+     let numStr = "";
+        for (let i = 0; i < s.length; i++) {
+            numStr += (s.charCodeAt(i) - 'a'.charCodeAt(0) + 1);
+        }
+
+        // Convert the string to the sum of its digits k times
+        while (k-- > 0) {
+            let sum = 0;
+            for (let i = 0; i < numStr.length; i++) {
+                sum += parseInt(numStr.charAt(i));
+            }
+            numStr = sum.toString();
+        }
+
+        return parseInt(numStr);
+};
+
+ 
