e9119faaba97db5675acdc9449b13f0f13b40e74
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-09-12 10:12:27 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (64.19%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int minBitFlips(int start, int goal) {
 
         String s = Integer.toBinaryString(start);
@@ -14,6 +14,23 @@ class Solution {
         // Compare bit by bit
         for (int i = 0; i < max; i++) {
             if (s.charAt(i) != g.charAt(i)) count++;
+        }
+
+        return count;
+    }
+}
+
+
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        // XOR the two numbers to get the bits that differ
+        int xor = start ^ goal;
+
+        // Count the number of 1s in the XOR result (i.e., the number of differing bits)
+        int count = 0;
+        while (xor > 0) {
+            count += xor & 1;  // Check the least significant bit
+            xor >>= 1;         // Shift right to check the next bit
         }
 
         return count;
