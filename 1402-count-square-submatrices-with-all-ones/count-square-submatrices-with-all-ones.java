ae8b5dd8378ac9cdd298e03d17a6b7beb7b5b52c
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2024-10-27 23:42:50 +0530

    Time: 8 ms (20.73%) | Memory: 56.3 MB (14.54%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -36,7 +36,7 @@ class Solution1 {
     }
 }
 
-class Solution {
+class Solution2 {
     public int countSquares(int[][] matrix) {
     int dp[][] = new int[matrix.length+1][matrix[0].length+1];
     int ans = 0; 
@@ -50,5 +50,34 @@ class Solution {
       }  
     }  
     return ans;  
+    }
+}
+
+class Solution {
+    public int countSquares(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }   
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int rst = 0;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (matrix[i][j] == 1) {
+                    int left = getNum(matrix, i - 1, j);
+                    int up = getNum(matrix, i, j - 1);
+                    int dia = getNum(matrix, i - 1, j - 1);
+                    matrix[i][j] += Math.min(Math.min(left, up), dia);
+                    rst += matrix[i][j];
+                }
+            }
+        }
+        return rst;
+    }
+    private int getNum(int[][] matrix, int x, int y) {
+        if (x < 0 || x >= matrix.length || y < 0 || y >= matrix[x].length) {
+            return 0;
+        }
+        return matrix[x][y];
     }
 }
