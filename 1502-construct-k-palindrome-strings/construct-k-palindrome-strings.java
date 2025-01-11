fa3c6af010fe92e9bf374ce48d712ab17075683f
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2025-01-11 21:13:20 +0530

    Time: 3 ms (100.00%) | Memory: 45.5 MB (45.32%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-class Solution {
+class Solution1 {
     public boolean canConstruct(String s, int k) {
         if (k > s.length()) {
             return false; // Impossible to create more palindromes than the length of the string
@@ -22,5 +22,27 @@ class Solution {
         
         // Check if the number of odd characters is less than or equal to k
         return oddCount <= k;
+    }
+}
+
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        // Handle edge cases
+        if (s.length() < k) return false;
+        if (s.length() == k) return true;
+
+
+        // Initialize oddCount as an integer bitmask
+        int oddCount = 0;
+
+
+        // Update the bitmask for each character in the string
+        for (char chr : s.toCharArray()) {
+            oddCount ^= 1 << (chr - 'a');
+        }
+
+
+        // Return if the number of odd frequencies is less than or equal to k
+        return Integer.bitCount(oddCount) <= k;
     }
 }
