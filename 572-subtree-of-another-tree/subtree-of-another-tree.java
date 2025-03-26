e7779fff7618cb6dafc073b831e4d61177f63d2a
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2025-03-26 18:58:02 +0530

    Time: 2 ms (97.26%) | Memory: 44.7 MB (30.54%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -1,3 +1,29 @@
+
+class Solution1 {
+    public boolean isSubtree(TreeNode root, TreeNode subroot) {
+         if(root==null){
+                    return false;
+                }
+                if(root.val==subroot.val){
+                    if(isidentical(root, subroot)) return true;
+                }
+
+                return isSubtree(root.left, subroot) || isSubtree(root.right, subroot);
+    }
+     public static Boolean isidentical(TreeNode root,TreeNode subroot){
+                if(root ==null && subroot ==null){
+                    return true;
+                }
+                else if(root ==null || subroot ==null ||root.val!=subroot.val){
+                    return false;
+                }
+                if(!isidentical(root.left, subroot.left)) return false;
+                if(!isidentical(root.right, subroot.right)) return false;
+                return true;
+            }
+           
+}
+
 class Solution {
     public boolean isSubtree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
