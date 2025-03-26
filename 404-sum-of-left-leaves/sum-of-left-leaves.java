2eb0b20dc6a4b3ff81e751263ac4d04be4b7a63b
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2025-03-26 17:52:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (5.18%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -1,11 +1,21 @@
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-        if(root == null) return 0;
-
-        if(root.left != null && root.left.left == null && root.left.right == null) {
-            return root.left.val + sumOfLeftLeaves(root.right);
+        if (root == null) {
+            return 0;
         }
-        
-        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        return helper(root);
+    }
+
+    private int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int currs = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            currs = root.left.val;
+        }
+
+        return currs + helper(root.left) + helper(root.right);
     }
 }
