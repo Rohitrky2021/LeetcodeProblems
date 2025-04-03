f22cfb8bbfed25b7861d0a059ee5321e6dc2392f
Rohit Yadav <102401490+Rohitrky2021@users.noreply.github.com>
2025-04-03 22:25:14 +0530

    Time: 204 ms (5.00%) | Memory: 89.2 MB (6.44%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public boolean isMatch(String s, String p) {
         
         int n = s.length();
@@ -41,6 +41,68 @@ class Solution {
         }
         else {
             dp[i][j] = 0;
+            return false;
+        }
+    }
+}
+
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        
+        // Initialize HashMap for memoization
+        HashMap<String, Boolean> map = new HashMap<>();
+        
+        // Start recursion from the end of both strings
+        return memo(s, p, n - 1, m - 1, map);
+    }
+
+    public static boolean memo(String s, String p, int i, int j, HashMap<String, Boolean> map) {
+        // Base cases
+        if (i < 0 && j < 0) {
+            return true; // Both strings exhausted
+        }
+        
+        if (i < 0 && j >= 0) {
+            // s is exhausted, check if remaining p is all '*'
+            for (int x = 0; x <= j; x++) {
+                if (p.charAt(x) != '*') return false;
+            }
+            return true;
+        } else if (j < 0 && i >= 0) {
+            // p is exhausted, s still has characters
+            return false;
+        }
+
+        // Create the key for HashMap
+        String key = i + " " + j;
+        
+        // Check if result is already memoized
+        if (map.containsKey(key)) {
+            return map.get(key);
+        }
+
+        // Case 1: Characters match or p has '?'
+        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
+            boolean val = memo(s, p, i - 1, j - 1, map);
+            map.put(key, val); // Store result
+            return val;
+        }
+        
+        // Case 2: p has '*'
+        else if (p.charAt(j) == '*') {
+            boolean val = (memo(s, p, i, j - 1, map) ||   // '*' matches empty
+                           memo(s, p, i - 1, j, map) ||   // '*' matches one or more
+                           memo(s, p, i - 1, j - 1, map)); // '*' matches single char
+            map.put(key, val); // Store result
+            return val;
+        }
+        
+        // Case 3: No match
+        else {
+            map.put(key, false); // Store false
             return false;
         }
     }
